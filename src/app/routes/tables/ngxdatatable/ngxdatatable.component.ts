12bf3ba1f7605473ece444132be47c9dc42e60ea
Alexander Tymchuk <a.tymchuk@crif.com>
2017-04-12 19:21:51 +0300

    feature: added flowtree component

--- src/app/routes/tables/ngxdatatable/ngxdatatable.component.ts
+++ src/app/routes/tables/ngxdatatable/ngxdatatable.component.ts
@@ -14,17 +14,17 @@ export class NgxDatatableComponent implements OnInit, AfterViewInit {
   @ViewChild(DatatableComponent, {read: ElementRef}) dataTableRef: ElementRef;
   @ViewChild(DatatableComponent) dataTable: DatatableComponent;
 
-  ngAfterViewInit() {
-    // set up the height of datatable - it does not work with height specified
-    const height = this.element.offsetHeight;
-    // this.dataTableRef.nativeElement.style.height = `${height}px`;
-    // this.dataTable.recalculate();
-    // this.dataTable.bodyHeight = 400;
-  }
-
   element: HTMLElement;
   rows: Array<any> = [];
   selected: Array<any> = [];
+  columns: Array<any> = [
+    { prop: 'id', name: "#", minWidth: 30, maxWidth: 70 },
+    { name: 'Name', width: 150 },
+    { name: 'Gender', minWidth: 80, maxWidth: 100 },
+    { name: 'Age', width: 50, maxWidth: 50 },
+    { name: 'City', prop: "address.city", minWidth: 200, maxWidth: 200 },
+    { name: 'State', prop: "address.state", minWidth: 200 },
+  ];
 
   constructor(private http: Http, element: ElementRef) {
 
@@ -37,16 +37,13 @@ export class NgxDatatableComponent implements OnInit, AfterViewInit {
         });
   }
 
-  public columns: Array<any> = [
-    { prop: 'id', name: "#", minWidth: 30, maxWidth: 70 },
-    { name: 'Name', width: 150 },
-    { name: 'Gender', minWidth: 80, maxWidth: 100 },
-    { name: 'Age', width: 50, maxWidth: 50 },
-    { name: 'City', prop: "address.city", minWidth: 200, maxWidth: 200 },
-    { name: 'State', prop: "address.state", minWidth: 200 },
-  ];
-
-  public length: number = 0;
+  ngAfterViewInit() {
+    // set up the height of datatable - it does not work with height specified
+    const height = this.element.offsetHeight;
+    // this.dataTableRef.nativeElement.style.height = `${height}px`;
+    // this.dataTable.recalculate();
+    // this.dataTable.bodyHeight = 400;
+  }
 
   public ngOnInit(): void {
     // this.onChangeTable(this.config);
