fac602169f4b7631e80be89f407cc4e65d6b2961
Dmitry Maltsev <d.maltsev@crif.com>
2017-04-25 11:48:17 +0300

    Added query builder and grids to menu
    #time 30m #refs WEB20-3

--- src/app/core/menu/menu.service.ts
+++ src/app/core/menu/menu.service.ts
@@ -3,9 +3,34 @@ import { Http } from '@angular/http';
 import { IMenuItem, IMenuApiResponse } from './menu.interface';
 
 const ADDITIONAL_MENU_ITEMS: Array<IMenuItem> = [{
-  icon: 'icon-graph',
+  text: 'Home',
+  link: '/home',
+  icon: 'icon-home'
+}, {
   text: 'Workflow',
-  link: '/workflow'
+  link: '/workflow',
+  icon: 'icon-graph'
+}, {
+  text: 'Query Builder',
+  link: '/query-builder',
+  icon: 'icon-list'
+}, {
+  text: 'Grids',
+  link: '/grid',
+  icon: 'icon-grid',
+  submenu: [{
+    text: 'Large dataset',
+    link: '/grid/large'
+  }, {
+    text: 'Sortable',
+    link: '/grid/sortable'
+  }, {
+    text: 'Reorderable',
+    link: '/grid/reorderable'
+  }, {
+    text: 'Groupable',
+    link: '/grid/groupable'
+  }]
 }];
 
 @Injectable()
@@ -33,6 +58,6 @@ export class MenuService {
   }
 
   private prepareMenu(response: IMenuApiResponse) {
-    this.menuItems = response.menu.concat(ADDITIONAL_MENU_ITEMS);
+    this.menuItems = ADDITIONAL_MENU_ITEMS.concat(response.menu);
   }
 }
