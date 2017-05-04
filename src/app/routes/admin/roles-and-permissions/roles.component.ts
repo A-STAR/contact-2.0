7072951b1cbcfebcf5e67e9aa25c8caafde72219
Dmitry Maltsev <d.maltsev@crif.com>
2017-05-04 11:24:35 +0300

    feat: popup form for roles editing
    #time 1h #refs WEB20-35

--- src/app/routes/admin/roles-and-permissions/roles.component.ts
+++ src/app/routes/admin/roles-and-permissions/roles.component.ts
@@ -1,14 +1,18 @@
 import { Component, EventEmitter, Output } from '@angular/core';
 import { FormControl, FormGroup, FormBuilder, Validators } from '@angular/forms';
 import { IDataSource } from '../../../shared/components/grid/grid.interface';
+import { IRoleRecord } from './roles.interface';
 
 @Component({
   selector: 'app-roles',
   templateUrl: './roles.component.html'
 })
 export class RolesComponent {
-  selectedRole: any = null;
   @Output() onSelect: EventEmitter<number> = new EventEmitter(false);
+
+  form: FormGroup;
+  editedRecord: any = null;
+  selectedRoleId: number = null;
 
   columns: Array<any> = [
     {
@@ -20,13 +24,13 @@ export class RolesComponent {
     {
       name: 'Название',
       prop: 'name',
-      maxWidth: 350,
+      maxWidth: 400,
     },
     {
       name: 'Комментарий',
       prop: 'obj_comment',
       width: 200,
-      maxWidth: 400,
+      maxWidth: 500,
     },
   ];
 
@@ -35,11 +39,48 @@ export class RolesComponent {
     dataKey: 'roles',
   };
 
-  parseFn(data) {
+  constructor(private formBuilder: FormBuilder) {
+  }
+
+  parseFn(data): Array<IRoleRecord> {
     return data.roles;
   }
 
-  onSelectedRowChange(event) {
-    this.onSelect.emit(event && event.id);
+  onSelectedRowChange(event: IRoleRecord) {
+    if (event && event.id && event.id !== this.selectedRoleId) {
+      this.selectedRoleId = event.id;
+      this.onSelect.emit(this.selectedRoleId);
+    }
+  }
+
+  onEdit(event: IRoleRecord) {
+    this.editedRecord = event;
+    // this.form = this.createForm();
+  }
+
+  onDisplayChange(event) {
+    if (!event) {
+      this.editedRecord = null;
+    }
+  }
+
+  save() {
+    this.editedRecord = null;
+  }
+
+  cancel() {
+    this.editedRecord = null;
+  }
+
+  createForm() {
+    return this.formBuilder.group({
+      id: new FormControl({ value: this.editedRecord.id, disabled: true }, Validators.required),
+      name: [ this.editedRecord.name, Validators.required ],
+      comment: [ this.editedRecord.obj_comment, Validators.required ],
+    });
+  }
+
+  get title() {
+    return this.editedRecord && this.editedRecord.name;
   }
 }
