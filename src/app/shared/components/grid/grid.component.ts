08d8f24f14d9ac4de59082b8e4217edb4367a7a7
Tymchuk Alexander <a.tymchuk@crif.com>
2017-05-05 17:28:47 +0300

    chore: added CRIF licence

--- src/app/shared/components/grid/grid.component.ts
+++ src/app/shared/components/grid/grid.component.ts
@@ -66,7 +66,7 @@ export class GridComponent implements OnInit, AfterViewInit {
     // 43px - tab height,
     // 2x15px - top & bottom padding around the grid
     // 8px => ? tbd
-    if (!this.styles) {
+    if (this.styles) {
       return;
     }
     const offset = 43 + 15 + 15 + 8;
