76be0b9456f9d9c2858e1f9ec6869a70498d08d1
a.poterenko <a.poterenko@crif.com>
2017-05-10 17:48:29 +0300

    feat: use permission service in grid permissions #refs WEB20-38

--- src/app/routes/admin/roles/permissions/permissions.service.ts
+++ src/app/routes/admin/roles/permissions/permissions.service.ts
@@ -1,12 +1,14 @@
 import { Injectable } from '@angular/core';
 
+import { UserPermissionsService } from '../../../../core/user/permissions/user-permissions.service';
 import { GridService } from '../../../../shared/components/grid/grid.service';
 import { IPermissionRole, IPermissionsRequest } from './permissions.interface';
 
 @Injectable()
 export class PermissionsService {
 
-  constructor(private gridService: GridService) {
+  constructor(private gridService: GridService,
+              private userPermissionsService: UserPermissionsService) {
   }
 
   public editPermission(role: IPermissionRole, permitId: number, request: IPermissionsRequest): Promise<any> {
@@ -19,5 +21,21 @@ export class PermissionsService {
 
   public addPermission(role: IPermissionRole, request: IPermissionsRequest): Promise<any> {
     return this.gridService.create(`/api/roles/{id}/permits`, role, request);
+  }
+
+  public canAddPermission(): boolean {
+    return this.userPermissionsService.hasPermission('PERMIT_ADD');
+  }
+
+  public canDeletePermission(): boolean {
+    return this.userPermissionsService.hasPermission('PERMIT_DELETE');
+  }
+
+  public canEditPermission(): boolean {
+    return this.userPermissionsService.hasPermission('PERMIT_EDIT');
+  }
+
+  public canShowPermission(): boolean {
+    return this.userPermissionsService.hasPermission('PERMIT_VIEW');
   }
 }
