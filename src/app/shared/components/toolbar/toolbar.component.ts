b55d242ec27a556622c8ae31fcde9c0467fb4f56
a.poterenko <a.poterenko@crif.com>
2017-05-10 15:37:30 +0300

    feat: review fixes #refs WEB20-38

--- src/app/shared/components/toolbar/toolbar.component.ts
+++ src/app/shared/components/toolbar/toolbar.component.ts
@@ -1,5 +1,6 @@
 import {Component, EventEmitter, Input, Output} from '@angular/core';
 import {IToolbarAction, ToolbarActionTypeEnum} from './toolbar.interface';
+import {IconsService} from "../../icons/icons.service";
 
 @Component({
   selector: 'app-toolbar',
@@ -11,23 +12,14 @@ export class ToolbarComponent {
   @Input() actions: IToolbarAction[];
   @Output() actionClick: EventEmitter<IToolbarAction> = new EventEmitter<IToolbarAction>(false);
 
+  constructor(private iconsService: IconsService) {
+  }
+
   onActionClick(action: IToolbarAction) {
     this.actionClick.emit(action);
   }
 
   toIconCls(action: IToolbarAction) {
-    // TODO Make it generic
-    switch (action.type) {
-      case ToolbarActionTypeEnum.CLONE:
-        return 'clone';
-      case ToolbarActionTypeEnum.EDIT:
-        return 'pencil';
-      case ToolbarActionTypeEnum.REMOVE:
-        return 'trash';
-      case ToolbarActionTypeEnum.ADD:
-        return 'plus';
-      default:
-        return 'default';
-    }
+    return this.iconsService.byIconCls(action.type);
   }
 }
---
+++ src/app/shared/icons/icons.service.ts
@@ -0,0 +1,17 @@
+import { Injectable } from '@angular/core';
+import { ToolbarActionTypeEnum } from '../components/toolbar/toolbar.interface';
+
+@Injectable()
+export class IconsService {
+
+  private ACTION_ICONS_MAP = new Map([
+    [ToolbarActionTypeEnum.CLONE, 'clone'],
+    [ToolbarActionTypeEnum.EDIT, 'pencil'],
+    [ToolbarActionTypeEnum.REMOVE, 'trash'],
+    [ToolbarActionTypeEnum.ADD, 'plus']
+  ]);
+
+  public fromActionType(actionType: ToolbarActionTypeEnum) {
+    return this.ACTION_ICONS_MAP[actionType] || `default-${actionType}`;
+  }
+}
