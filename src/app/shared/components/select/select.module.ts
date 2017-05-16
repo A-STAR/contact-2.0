076eec1c0ef4a4f366e12a038682e341633d6bde
a.poterenko <a.poterenko@crif.com>
2017-05-16 14:16:35 +0300

    fix: remove unnecessary import  #refs WEB20-78

--- src/app/shared/components/select/select.module.ts
+++ src/app/shared/components/select/select.module.ts
@@ -1,13 +1,12 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { ReactiveFormsModule } from '@angular/forms';
 
 import { SelectComponent } from './select';
 import { HighlightPipe } from './select-pipes';
 import { OffClickDirective } from './off-click';
 
 @NgModule({
-  imports: [CommonModule, ReactiveFormsModule],
+  imports: [CommonModule],
   declarations: [SelectComponent, HighlightPipe, OffClickDirective],
   exports: [SelectComponent, HighlightPipe, OffClickDirective]
 })
