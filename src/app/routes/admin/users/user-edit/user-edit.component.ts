c919495a1f8a65eb3000a37c1890df7e7745cc2a
Dmitry Maltsev <d.maltsev@crif.com>
2017-05-17 13:07:13 +0300

    feat: roles select input
    #refs WEB20-48

--- src/app/routes/admin/users/user-edit/user-edit.component.ts
+++ src/app/routes/admin/users/user-edit/user-edit.component.ts
@@ -1,8 +1,11 @@
 import { Component, EventEmitter, Input, Output, OnInit, ViewChild } from '@angular/core';
 
+import { GridService } from '../../../../shared/components/grid/grid.service';
 import { DynamicFormComponent } from '../../../../shared/components/form/dynamic-form/dynamic-form.component';
 import { IDynamicFormControl } from '../../../../shared/components/form/dynamic-form/dynamic-form-control.interface';
+import { SelectionActionTypeEnum } from '../../../../shared/components/form/select/select-interfaces';
 import { IUser } from '../users.interface';
+import { IRolesResponse } from '../../roles/roles/roles.interface';
 
 @Component({
   selector: 'app-user-edit',
@@ -15,7 +18,11 @@ export class UserEditComponent implements OnInit {
 
   controls: Array<IDynamicFormControl>;
 
+  data: any;
+
   error: string = null;
+
+  constructor(private gridService: GridService) {}
 
   get canSubmit(): boolean {
     return this.form.canSubmit;
@@ -27,6 +34,7 @@ export class UserEditComponent implements OnInit {
 
   ngOnInit(): void {
     this.controls = this.getControls();
+    this.data = this.getData();
   }
 
   onDisplayChange(event: boolean): void {
@@ -44,27 +52,44 @@ export class UserEditComponent implements OnInit {
   }
 
   private getControls(): Array<IDynamicFormControl> {
+    const roleSelectOptions = {
+      cachingOptions: true,
+      lazyOptions: this.gridService
+        .read('/api/roles')
+        .map((data: IRolesResponse) => data.roles.map(role => ({ label: role.name, value: role.id }))),
+      optionsActions: [
+        { text: 'Выберите роль', type: SelectionActionTypeEnum.SORT}
+      ]
+    };
+
     return [
       { label: 'Фамилия', controlName: 'lastName', type: 'text', required: true },
-      { label: 'Имя', controlName: 'firstName', type: 'text', required: true },
-      { label: 'Отчество', controlName: 'middleName', type: 'text', required: true },
+      { label: 'Имя', controlName: 'firstName', type: 'text' },
+      { label: 'Отчество', controlName: 'middleName', type: 'text' },
       // TODO: insert photo upload control here
       // TODO: do we need separate type 'checkbox' in addition to 'boolean'?
       { label: 'Блокирован', controlName: 'isBlocked', type: 'boolean', required: true },
       { label: 'Логин', controlName: 'login', type: 'text', required: true },
       { label: 'Пароль', controlName: 'password', type: 'text', required: true },
-      { label: 'Роль', controlName: 'roleId', type: 'text', required: true },
-      { label: 'Должность', controlName: 'position', type: 'text', required: true },
-      { label: 'Дата начала работы', controlName: 'startWorkDate', type: 'text', required: true },
-      { label: 'Дата окончания работы', controlName: 'endWorkDate', type: 'text', required: true },
-      { label: 'Мобильный телефон', controlName: 'mobPhone', type: 'text', required: true },
-      { label: 'Рабочий телефон', controlName: 'workPhone', type: 'text', required: true },
-      { label: 'Внутренний номер', controlName: 'intPhone', type: 'text', required: true },
-      { label: 'Email', controlName: 'email', type: 'text', required: true },
+      { label: 'Роль', controlName: 'roleId', type: 'select', required: true, ...roleSelectOptions },
+      { label: 'Должность', controlName: 'position', type: 'text' },
+      { label: 'Дата начала работы', controlName: 'startWorkDate', type: 'text' },  // calendar
+      { label: 'Дата окончания работы', controlName: 'endWorkDate', type: 'text' },  // calendar
+      { label: 'Мобильный телефон', controlName: 'mobPhone', type: 'text' },
+      { label: 'Рабочий телефон', controlName: 'workPhone', type: 'text' },
+      { label: 'Внутренний номер', controlName: 'intPhone', type: 'text' },
+      { label: 'Email', controlName: 'email', type: 'text' },
       { label: 'Рабочий адрес', controlName: 'address', type: 'text', required: true },
-      { label: 'Язык', controlName: 'langCode', type: 'text', required: true },
-      { label: 'Комментарий', controlName: 'comment', type: 'textarea', required: true },
+      { label: 'Язык', controlName: 'langCode', type: 'select', required: true },  // select
+      { label: 'Комментарий', controlName: 'comment', type: 'textarea' },
     ];
+  }
+
+  private getData(): any {
+    return {
+      ...this.user,
+      roleId: [{ value: this.user.roleId }]  // FIXME: add label
+    };
   }
 
   private close(): void {
