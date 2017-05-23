165a537d23d681b095c99edcde11fd6b7cf42ea7
Dmitry Maltsev <d.maltsev@crif.com>
2017-05-23 11:06:05 +0300

    feat: employees grid & form
    #refs WEB20-57

--- src/app/routes/admin/organizations/employees/employee-edit/employee-edit.component.ts
+++ src/app/routes/admin/organizations/employees/employee-edit/employee-edit.component.ts
@@ -1,10 +1,9 @@
 import { Component } from '@angular/core';
 
-import { IRolesResponse } from '../../../roles/roles/roles.interface';
+import { UserPermissionsService } from '../../../../../core/user/permissions/user-permissions.service';
 import { GridService } from '../../../../../shared/components/grid/grid.service';
 import { EntityBaseComponent } from '../../../../../shared/components/entity/edit/entity.base.component';
 import { IDynamicFormControl } from '../../../../../shared/components/form/dynamic-form/dynamic-form-control.interface';
-import { SelectionActionTypeEnum } from '../../../../../shared/components/form/select/select-interfaces';
 import { IEmployee } from '../../organizations.interface';
 
 @Component({
@@ -12,48 +11,42 @@ import { IEmployee } from '../../organizations.interface';
   templateUrl: './employee-edit.component.html'
 })
 export class EmployeeEditComponent extends EntityBaseComponent<IEmployee> {
-  constructor(private gridService: GridService) {
+  private canEdit = false;
+
+  constructor(private gridService: GridService, private userPermissionsService: UserPermissionsService) {
     super();
+    this.canEdit = this.userPermissionsService.hasPermission('ORGANIZATION_EDIT');
+  }
+
+  get formData(): IEmployee {
+    return {
+      ...this.editedEntity,
+      fullName: `${this.editedEntity.lastName || ''} ${this.editedEntity.firstName || ''} ${this.editedEntity.middleName || ''}`
+    };
   }
 
   protected getControls(): Array<IDynamicFormControl> {
     const roleSelectOptions = {
-      cachingOptions: true,
-      lazyOptions: this.gridService
-        .read('/api/roles')
-        .map((data: IRolesResponse) => data.roles.map(role => ({ label: role.name, value: role.id }))),
-      optionsActions: [
-        { text: 'users.select.role.title', type: SelectionActionTypeEnum.SORT}
+      options: [
+        { value: 1, label: 'Сотрудник' },
+        { value: 2, label: 'Руководитель' },
+        { value: 3, label: 'Заместитель' },
+        { value: 4, label: 'Куратор' },
       ]
     };
 
     return [
-      { label: 'id', controlName: 'userId', type: 'hidden', required: true },
-      { label: 'users.edit.lastName', controlName: 'lastName', type: 'text', required: true },
-      { label: 'users.edit.firstName', controlName: 'firstName', type: 'text' },
-      { label: 'users.edit.middleName', controlName: 'middleName', type: 'text' },
+      { label: 'organizations.employees.edit.fullName', controlName: 'fullName', type: 'text' },
       { label: 'users.edit.position', controlName: 'position', type: 'text' },
       { label: 'users.edit.email', controlName: 'email', type: 'text' },
       { label: 'users.edit.mobPhone', controlName: 'mobPhone', type: 'text' },
       { label: 'users.edit.workPhone', controlName: 'workPhone', type: 'text' },
       { label: 'users.edit.intPhone', controlName: 'intPhone', type: 'text' },
-      /*
-      {
-        label: 'users.edit.role',
-        controlName: 'roleCode',
-        type: 'select',
-        required: true,
-        loadLazyItemsOnInit: true,
-        // FIXME
-        disabled: false,
-        ...roleSelectOptions
-      },
-      */
-      { label: 'users.edit.comment', controlName: 'comment', type: 'text', required: true }
+      { label: 'users.edit.role', controlName: 'roleCode', type: 'select', required: true, disabled: !this.canEdit, ...roleSelectOptions },
+      { label: 'users.edit.comment', controlName: 'comment', type: 'text', required: true, disabled: !this.canEdit }
     ].map((control: IDynamicFormControl) => ({
       ...control,
-      // FIXME
-      disabled: false
+      disabled: control.hasOwnProperty('disabled') ? control.disabled : true
     }));
   }
 }
--- src/app/routes/admin/organizations/employees/employees.component.ts
+++ src/app/routes/admin/organizations/employees/employees.component.ts
@@ -1,8 +1,11 @@
 import { Component } from '@angular/core';
+import { TranslateService } from '@ngx-translate/core';
 
 import { IDataSource } from '../../../../shared/components/grid/grid.interface';
 import { IToolbarAction, ToolbarActionTypeEnum } from '../../../../shared/components/toolbar/toolbar.interface';
 import { GridEntityComponent } from '../../../../shared/components/entity/grid.entity.component';
+import { GridColumnDecoratorService } from '../../../../shared/components/grid/grid.column.decorator.service';
+import { OrganizationsService } from '../organizations.service';
 import { IEmployee } from '../organizations.interface';
 
 @Component({
@@ -10,10 +13,6 @@ import { IEmployee } from '../organizations.interface';
   templateUrl: './employees.component.html'
 })
 export class EmployeesComponent extends GridEntityComponent<IEmployee> {
-  get info(): string {
-    return JSON.stringify(this.masterEntity, null, 2);
-  }
-
   bottomActions: Array<IToolbarAction> = [
     { text: 'toolbar.action.add', type: ToolbarActionTypeEnum.ADD, visible: true, permission: 'ORGANIZATION_EDIT' },
     { text: 'toolbar.action.edit', type: ToolbarActionTypeEnum.EDIT, visible: false, permission: 'ORGANIZATION_EDIT' },
@@ -26,11 +25,26 @@ export class EmployeesComponent extends GridEntityComponent<IEmployee> {
   ];
 
   columns: Array<any> = [
-    { prop: 'lastName', minWidth: 30, maxWidth: 70 },
-    { prop: 'firstName', minWidth: 30, maxWidth: 70 },
-    { prop: 'middleName', minWidth: 30, maxWidth: 70 },
-    { prop: 'roleCode' },
-    { prop: 'isBlocked' },
+    this.columnDecoratorService.decorateColumn(
+      { prop: 'fullName', minWidth: 200 },
+      (employee: IEmployee) => `${employee.lastName || ''} ${employee.firstName || ''} ${employee.middleName || ''}`
+    ),
+    { prop: 'position' },
+    this.columnDecoratorService.decorateColumn(
+      { prop: 'roleCode', minWidth: 100 }, (column, roleCode: number) => {
+        switch (roleCode) {
+          case 1: return 'Сотрудник';
+          case 2: return 'Руководитель';
+          case 3: return 'Заместитель';
+          case 4: return 'Куратор';
+        }
+        return roleCode;
+      }
+    ),
+    this.columnDecoratorService.decorateColumn(
+      // TODO: display column depending on filter
+      { prop: 'isBlocked', minWidth: 100 }, ({ isBlocked }) => this.transformIsBlocked(isBlocked)
+    ),
   ];
 
   dataSource: IDataSource = {
@@ -38,4 +52,18 @@ export class EmployeesComponent extends GridEntityComponent<IEmployee> {
     // update: '/api/terms',
     dataKey: 'users',
   };
+
+  constructor(
+    private columnDecoratorService: GridColumnDecoratorService,
+    private organizationsService: OrganizationsService,
+    private translateService: TranslateService) {
+
+    super();
+  }
+
+  transformIsBlocked(isBlocked: number): string {
+    // TODO: render checkbox
+    return this.translateService.instant(isBlocked ? 'default.yesNo.Yes' : 'default.yesNo.No');
+  }
+
 }
