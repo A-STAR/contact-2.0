effa196348fc8dc973ffebd978dcc4d5484b1114
Tymchuk Alexander <a.tymchuk@crif.com>
2017-05-30 14:19:35 +0300

    fix: component names & tab styles #refs WEB20-15'

--- src/app/layout/layout.component.ts
+++ src/app/layout/layout.component.ts
@@ -12,12 +12,12 @@ import { ITab } from '../shared/components/content-tabstrip/tab/content-tab.inte
 })
 export class LayoutComponent {
   titles = {
-    ConstantsComponent: 'constants.title',
-    DashboardComponent: 'sidebar.nav.menu.HOME',
-    DictAndTermsComponent: 'sidebar.nav.menu.DICTIONARIES',
-    OrganizationsComponent: 'organizations.title',
-    RolesAndPermissionsComponent: 'sidebar.nav.menu.ROLES_AND_PERMISSIONS',
-    UsersComponent: 'users.title',
+    ConstantsComponent: 'CONSTANTS',
+    DashboardComponent: 'HOME',
+    DictAndTermsComponent: 'DICTIONARIES',
+    OrganizationsComponent: 'DEPARTMENTS',
+    RolesAndPermissionsComponent: 'ROLES_AND_PERMISSIONS',
+    UsersComponent: 'USERS',
   };
 
   constructor(
@@ -35,9 +35,9 @@ export class LayoutComponent {
 
   activate(config: any): void {
     const { component, factory, injector } = config;
-    // console.log('activated', component.name);
+    console.log('activate', component.COMPONENT_NAME);
     const path: string = this.router.url;
-    const title: string = this.titles[component.COMPONENT_NAME] || 'Title';
+    const title: string = this.titles[`sidebar.nav.menu.${component.COMPONENT_NAME}`] || 'Title';
 
     const tab = { title, active: true, component, path, factory, injector };
     this.tabService.addTab(tab);
