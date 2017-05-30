586ce0afc493b89f414e5888659e5bc24ef208f9
Tymchuk Alexander <a.tymchuk@crif.com>
2017-05-30 14:32:46 +0300

    fix: component names #refs WEB20-15'

--- src/app/layout/layout.component.ts
+++ src/app/layout/layout.component.ts
@@ -37,8 +37,8 @@ export class LayoutComponent {
     const { component, factory, injector } = config;
     console.log('activate', component.COMPONENT_NAME);
     const path: string = this.router.url;
-    const title: string = this.titles[`sidebar.nav.menu.${component.COMPONENT_NAME}`] || 'Title';
-
+    const key = this.titles[component.COMPONENT_NAME];
+    const title: string = key ? `sidebar.nav.menu.${key}` : 'Title';
     const tab = { title, active: true, component, path, factory, injector };
     this.tabService.addTab(tab);
   }
