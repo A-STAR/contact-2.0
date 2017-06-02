74e0ae7fd84295c3a5326aa0535ab8284fd0a5b6
a.poterenko <a.poterenko@crif.com>
2017-06-02 18:03:50 +0300

    fix: Action logs filter #refs WEB20-109

--- src/app/core/state/root.reducer.ts
+++ src/app/core/state/root.reducer.ts
@@ -2,6 +2,7 @@ import { employeesReducer } from '../../routes/admin/organizations/employees/emp
 import { organizationsReducer } from '../../routes/admin/organizations/organizations-tree/organizations.reducer';
 import { notificationReducer } from '../notifications/notifications.reducer';
 import { permissionReducer } from '../permissions/permissions.reducer';
+import { actionsLogReducer } from '../../routes/admin/actions-log/actions-log.reducer';
 
 export const rootReducer = {
   // TODO: rename notificationService -> notifications
