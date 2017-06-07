bec1b3368d5953daeb73eefbb278f87eea2db998
Dmitry Maltsev <d.maltsev@crif.com>
2017-06-07 19:13:59 +0300

    fix: removed unnecessary interface
    #refs WEB20-89 WEB20-90

--- src/app/core/dictionaries/dictionaries.interface.ts
+++ src/app/core/dictionaries/dictionaries.interface.ts
@@ -40,11 +40,6 @@ export interface IDictionariesState {
   dialogAction: DictionariesDialogActionEnum;
 }
 
-export class EntityTranslationsConstants {
-  static SPEC_DICT_NAME = 3;
-  static SPEC_TERM_NAME = 12;
-}
-
 export interface IEntityTranslation {
   languageId: number;
   value: string;
