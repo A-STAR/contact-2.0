c2eadf9228b476a288c041cb5f50af4d590382d0
a.poterenko <a.poterenko@crif.com>
2017-06-28 15:29:50 +0300

    feat: tab-strip usage

--- src/app/shared/components/tabstrip/tabstrip.component.ts
+++ src/app/shared/components/tabstrip/tabstrip.component.ts
@@ -1,17 +1,42 @@
-import { Component, ContentChildren, QueryList, AfterContentInit } from '@angular/core';
+import {
+  Component,
+  ContentChildren,
+  QueryList,
+  AfterContentInit,
+  ElementRef,
+  AfterViewInit,
+  ChangeDetectionStrategy,
+  ChangeDetectorRef,
+  ViewChild,
+  DoCheck,
+} from '@angular/core';
+
+import { PerfectScrollbarComponent } from 'ngx-perfect-scrollbar';
 
 import { TabComponent } from './tab.component';
 
 @Component({
   selector: 'app-tabstrip',
   templateUrl: 'tabstrip.component.html',
-  styleUrls: ['./tabstrip.component.scss']
+  styleUrls: ['./tabstrip.component.scss'],
+  changeDetection: ChangeDetectionStrategy.OnPush,
 })
 
-export class TabstripComponent implements AfterContentInit {
-  @ContentChildren(TabComponent) tabs: QueryList<TabComponent>;
+export class TabstripComponent implements AfterContentInit, AfterViewInit, DoCheck {
 
-  constructor() { }
+  @ContentChildren(TabComponent) tabs: QueryList<TabComponent>;
+  @ViewChild('ps') ps: PerfectScrollbarComponent;
+  isToolsVisible: boolean;
+
+  constructor(
+    private elementRef: ElementRef,
+    private detectorRef: ChangeDetectorRef) {
+  }
+
+  ngDoCheck(): void {
+    this.ps.ngDoCheck(); // TODO Make github issue request
+    this.refreshTools();
+  }
 
   ngAfterContentInit(): void {
     const activeTabs = this.tabs.filter(tab => tab.active);
@@ -20,6 +45,10 @@ export class TabstripComponent implements AfterContentInit {
     if (!activeTabs.length) {
       this.selectTab(this.tabs.first);
     }
+  }
+
+  ngAfterViewInit(): void {
+    this.refreshTools();
   }
 
   selectTab(tab: TabComponent): void {
@@ -32,5 +61,23 @@ export class TabstripComponent implements AfterContentInit {
   closeTab(tab: TabComponent): void {
     const index = this.tabs.toArray().findIndex(el => el === tab);
     tab.onClose.emit(index);
+  }
+
+  refreshTools(): void {
+    const xRail = this.elementRef.nativeElement.querySelector('.ps__scrollbar-x-rail');
+    const yRail = this.elementRef.nativeElement.querySelector('.ps__scrollbar-y-rail');
+    if (xRail && yRail) {
+      // jQuery
+      this.isToolsVisible = $(xRail).is(':visible') || $(yRail).is(':visible');
+      this.detectorRef.detectChanges();
+    }
+  }
+
+  onRight(): void {
+    this.ps.scrollToRight();
+  }
+
+  onLeft(): void {
+    this.ps.scrollToLeft();
   }
 }
