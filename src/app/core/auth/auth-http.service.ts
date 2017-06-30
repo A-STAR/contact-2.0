0fa87f629c6f325fe163134b107e83ce7efaacfa
Dmitry Maltsev <d.maltsev@crif.com>
2017-06-30 15:24:22 +0300

    chore: added type

--- src/app/core/auth/auth-http.service.ts
+++ src/app/core/auth/auth-http.service.ts
@@ -6,7 +6,7 @@ import { getToken } from '../auth/auth.service';
  * Must be an exported non-arrow function to pass static analysis.
  * See https://medium.com/@isaacplmann/making-your-angular-2-library-statically-analyzable-for-aot-e1c6f3ebedd5
  */
-export function authHttpServiceFactory(http: Http, options: RequestOptions): any {
+export function authHttpServiceFactory(http: Http, options: RequestOptions): AuthHttp {
   return new AuthHttp(new AuthConfig({
     tokenGetter: getToken,
     noJwtError: true
