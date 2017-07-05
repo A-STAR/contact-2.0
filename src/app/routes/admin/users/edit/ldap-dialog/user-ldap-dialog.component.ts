f3b30804c56f7d783fd9bf52e75e37b68a5b3193
Dmitry Maltsev <d.maltsev@crif.com>
2017-07-05 11:19:41 +0300

    feat: ldap grids boilerplate

--- src/app/routes/admin/users/edit/ldap-dialog/user-ldap-dialog.component.ts
+++ src/app/routes/admin/users/edit/ldap-dialog/user-ldap-dialog.component.ts
@@ -1,8 +1,38 @@
 import { Component } from '@angular/core';
+import { Observable } from 'rxjs/Observable';
+
+import { IGridColumn } from '../../../../../shared/components/grid/grid.interface';
 
 @Component({
   selector: 'app-user-ldap-dialog',
   templateUrl: './user-ldap-dialog.component.html'
 })
 export class UserLdapDialogComponent {
+  groupsColumns: Array<IGridColumn> = [
+    { prop: 'name' },
+    { prop: 'comment' },
+  ];
+
+  usersColumns: Array<IGridColumn> = [
+    { prop: 'name' },
+    { prop: 'fullName' },
+    { prop: 'isBlocked' },
+    { prop: 'comment' },
+  ];
+
+  groups$ = Observable.of([]);
+
+  users$ = Observable.of([]);
+
+  constructor() {
+    //
+  }
+
+  onGroupSelect(event: any): void {
+    //
+  }
+
+  onUserSelect(event: any): void {
+    //
+  }
 }
