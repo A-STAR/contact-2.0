fd9c76d2bcb8b0541d140f406242be73fb7afb86
Dmitry Maltsev <d.maltsev@crif.com>
2017-07-05 18:24:42 +0300

    feat: dropdown

--- src/app/shared/components/dropdown/dropdown.component.ts
+++ src/app/shared/components/dropdown/dropdown.component.ts
@@ -1,9 +1,41 @@
-import { Component } from '@angular/core';
+import { Component, ElementRef, HostListener, ViewChild } from '@angular/core';
 
 @Component({
   selector: 'app-dropdown',
-  templateUrl: './dropdown.component.html'
+  templateUrl: './dropdown.component.html',
+  styleUrls: [ './dropdown.component.scss' ]
 })
 export class DropdownComponent {
+  private _isOpen = false;
 
+  @ViewChild('trigger') trigger: ElementRef;
+
+  @HostListener('document:click', ['$event'])
+  onDocumentClick(event: MouseEvent): void {
+    if (!this.containsTarget(this.element, event) && !this.containsTarget(this.trigger, event)) {
+      this._isOpen = false;
+    }
+  };
+
+  constructor(private element: ElementRef) {}
+
+  get isOpen(): boolean {
+    return this._isOpen;
+  }
+
+  toggle(): void {
+    this._isOpen = !this._isOpen;
+  }
+
+  open(): void {
+    this._isOpen = true;
+  }
+
+  close(): void {
+    this._isOpen = false;
+  }
+
+  private containsTarget(element: ElementRef, event: MouseEvent): boolean {
+    return element.nativeElement.contains(event.target);
+  }
 }
