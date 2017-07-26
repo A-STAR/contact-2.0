c300cd1972fd91c76e38db2418f5f2e32bd16ff5
Dmitry Maltsev <d.maltsev@crif.com>
2017-07-26 14:26:28 +0300

    feat: debt processing metadata
    #refs WEB20-170

--- src/app/routes/workplaces/debt-processing/debt-processing.component.ts
+++ src/app/routes/workplaces/debt-processing/debt-processing.component.ts
@@ -1,9 +1,32 @@
-import { Component } from '@angular/core';
+import { ChangeDetectionStrategy, Component, ViewEncapsulation } from '@angular/core';
+import { Observable } from 'rxjs/Observable';
+import 'rxjs/add/observable/of';
 
 @Component({
   selector: 'app-debt-processing',
-  templateUrl: './debt-processing.component.html'
+  templateUrl: './debt-processing.component.html',
+  styleUrls: [ './debt-processing.component.scss' ],
+  encapsulation: ViewEncapsulation.None,
+  changeDetection: ChangeDetectionStrategy.OnPush
 })
 export class DebtProcessingComponent {
   static COMPONENT_NAME = 'DebtProcessingComponent';
+
+  page$ = Observable.of(1);
+  pageSize$ = Observable.of(100);
+  rows$ = Observable.of([{ debtId: 1 }]);
+  rowCount$ = this.rows$.map(rows => rows.length);
+  selected$ = Observable.of([]);
+
+  onRequestData(event: any): void {
+    console.log('onRequestData');
+  }
+
+  onFilter(event: any): void {
+    console.log('onFilter');
+  }
+
+  onSelect(event: any): void {
+    console.log('onSelect');
+  }
 }
