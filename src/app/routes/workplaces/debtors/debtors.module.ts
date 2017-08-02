69da5d22ce9370f60cbe65a7122f6d6cfce38ae3
Dmitry Maltsev <d.maltsev@crif.com>
2017-08-02 15:37:40 +0300

    feat: email & phone cards routes
    #refs WEB20-185 WEB20-187

--- src/app/routes/workplaces/debtors/debtors.module.ts
+++ src/app/routes/workplaces/debtors/debtors.module.ts
@@ -12,12 +12,18 @@ import { DebtorModule } from './debtor/debtor.module';
 
 import { DebtorComponent } from './debtor/debtor.component';
 import { DebtorAddressComponent } from './debtor/address/address.component';
+import { DebtorEmailComponent } from './debtor/email/email.component';
+import { DebtorPhoneComponent } from './debtor/phone/phone.component';
 
 const routes: Routes = [
   { path: '', component: DebtorsComponent },
   { path: ':id', component: DebtorComponent },
   { path: ':id/address/create', component: DebtorAddressComponent },
-  { path: ':id/address/:addressId', component: DebtorAddressComponent }
+  { path: ':id/address/:addressId', component: DebtorAddressComponent },
+  { path: ':id/email/create', component: DebtorEmailComponent },
+  { path: ':id/email/:emailId', component: DebtorEmailComponent },
+  { path: ':id/phone/create', component: DebtorPhoneComponent },
+  { path: ':id/phone/:phoneId', component: DebtorPhoneComponent }
 ];
 
 @NgModule({
