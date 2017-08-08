bc3fd04b4e7871787a84d7ce5f9a70badad14fc2
Dmitry Maltsev <d.maltsev@crif.com>
2017-08-08 14:07:33 +0300

    feat: debts grid boilerplate
    #refs WEB20-189

--- src/app/shared/gui-objects/widgets/debt/grid/debt-grid.component.ts
+++ src/app/shared/gui-objects/widgets/debt/grid/debt-grid.component.ts
@@ -1,10 +1,41 @@
 import { ChangeDetectionStrategy, Component } from '@angular/core';
 
+import { IDebt } from '../debt.interface';
+import { IGridColumn } from '../../../../../shared/components/grid/grid.interface';
+
 @Component({
   selector: 'app-debt-grid',
   templateUrl: './debt-grid.component.html',
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class DebtGridComponent {
+  get canDisplayGrid(): boolean {
+    return true;
+  }
 
+  columns: Array<IGridColumn> = [
+    { prop: 'id' },
+    { prop: 'creditTypeCode' },
+    { prop: 'creditName' },
+    { prop: 'contract' },
+    { prop: 'statusCode' },
+    { prop: 'creditStartDate' },
+    { prop: 'currencyId' },
+    { prop: 'debtSum' },
+    { prop: 'totalSum' },
+    { prop: 'dpd' },
+    { prop: 'portfolio' },
+    { prop: 'bank' },
+    { prop: 'debtReasonCode' },
+  ];
+
+  debts: Array<IDebt> = [];
+
+  onDoubleClick(debt: IDebt): void {
+
+  }
+
+  onSelect(debt: IDebt): void {
+
+  }
 }
