407d8d089826a5c2188860cb9548bab24d2de7ce
Alexander tymchuk <a.tymchuk@crif.com>
2017-09-01 14:10:09 +0300

    feat: scaffold the debtor's action-log component #refs WEB20-250

---
+++ src/app/routes/workplaces/debt-processing/debtor/action-log/action-log.service.ts
@@ -0,0 +1,30 @@
+import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs/Observable';
+import { Action } from '@ngrx/store';
+
+import { IAGridRequestParams, IAGridResponse } from '../../../../../shared/components/grid2/grid2.interface';
+import { IDebtorActionLog } from './action-log.interface';
+
+import { DataService } from '../../../../../core/data/data.service';
+import { GridService } from '../../../../../shared/components/grid/grid.service';
+import { NotificationsService } from '../../../../../core/notifications/notifications.service';
+
+import { FilterObject } from '../../../../../shared/components/grid2/filter/grid-filter';
+
+@Injectable()
+export class ActionLogService {
+
+  constructor(
+    private dataService: DataService,
+    private gridService: GridService,
+    private notifications: NotificationsService,
+  ) {}
+
+  fetch(filters: FilterObject, params: IAGridRequestParams): Observable<IAGridResponse<IDebtorActionLog> | Action> {
+    const request = this.gridService.buildRequest(params, filters);
+
+    return this.dataService
+      .create('/list?name=debtsprocessingall', {}, request)
+      .catch(this.notifications.error('errors.default.read').entity('entities.actionsLog.gen.plural').callback());
+  }
+}
