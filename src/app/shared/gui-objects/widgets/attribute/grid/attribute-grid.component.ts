959bdbfd9da35ca874997ca9fc1fe02a58089684
Dmitry Maltsev <d.maltsev@crif.com>
2017-09-12 18:31:00 +0300

    feat: unique ids
    #refs WEB20-285 WEB286

--- src/app/shared/gui-objects/widgets/attribute/grid/attribute-grid.component.ts
+++ src/app/shared/gui-objects/widgets/attribute/grid/attribute-grid.component.ts
@@ -24,19 +24,19 @@ export class AttributeGridComponent {
     return this._rows;
   }
 
-  private generateGridTreeRows(length: number): Array<IGridTreeRow<IAttribute>> {
-    return Array(length).fill(null).map((_, i) => this.generateRow(i));
+  private generateGridTreeRows(length: number, parentId: number = 0, parentLength: number = 0): Array<IGridTreeRow<IAttribute>> {
+    return Array(length).fill(null).map((_, i) => this.generateRow(i, length, parentId, parentLength));
   }
 
-  private generateRow(i: number): IGridTreeRow<IAttribute> {
-    const id = i + 1;
+  private generateRow(i: number, length: number, parentId: number, parentLength: number): IGridTreeRow<IAttribute> {
+    const id = parentLength * parentId + i + 1;
     return {
       data: {
         id,
         name: `Item #${id}`,
         type: ['Red', 'Green', 'Blue', 'Yellow', 'Black'][this.random(5)]
       },
-      children: this.random(10) === 0 ? this.generateGridTreeRows(5) : undefined
+      children: this.random(10) === 0 ? this.generateGridTreeRows(5, id, length) : undefined
     };
   }
 
