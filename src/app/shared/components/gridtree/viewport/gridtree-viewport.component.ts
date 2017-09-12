82ee8eecf2451fbef575fba7f1393aa4401d8fc3
Dmitry Maltsev <d.maltsev@crif.com>
2017-09-12 12:00:14 +0300

    feat: basic row dragging
    #refs WEB20-285 WEB20-286

--- src/app/shared/components/gridtree/viewport/gridtree-viewport.component.ts
+++ src/app/shared/components/gridtree/viewport/gridtree-viewport.component.ts
@@ -14,29 +14,41 @@ export class GridTreeViewportComponent<T> {
   @Input() height: number;
   @Input() rows: Array<IGridTreeRow<T>>;
 
-  private rowEvent: IGridTreeRowEvent<T>;
+  private _draggedRow: IGridTreeRow<T>;
+  private _y0: number;
+  private _y1: number;
 
   constructor(private cdRef: ChangeDetectorRef) {}
 
   get viewportRows(): Array<IGridTreeRow<T>> {
-    return this.rows.filter(row => !this.rowEvent || row !== this.rowEvent.row);
+    return this.rows.filter(row => row !== this._draggedRow);
   }
 
   get draggedRow(): IGridTreeRow<T> {
-    return this.rowEvent && this.rowEvent.row;
+    return this._draggedRow;
+  }
+
+  get draggedRowStyle(): Object {
+    return {
+      top: `${this._y1 - this._y0}px`
+    };
   }
 
   onMouseDown(rowEvent: IGridTreeRowEvent<T>): void {
-    this.rowEvent = rowEvent;
-    console.log(rowEvent.event.clientX, rowEvent.event.clientY);
-    // console.log((event.target as any).closest('.gridtree-row'));
+    if (rowEvent.event.button === 0) {
+      this._draggedRow = rowEvent.row;
+      this._y0 = rowEvent.event.clientY - ((rowEvent.event.target as any).closest('.gridtree-row') as any).offsetTop;
+      this.cdRef.markForCheck();
+    }
   }
 
   onMouseMove(event: MouseEvent): void {
-    // console.log(event);
+    this._y1 = event.clientY;
+    this.cdRef.markForCheck();
   }
 
   onMouseUp(event: MouseEvent): void {
-    // this.rowEvent = null;
+    this._draggedRow = null;
+    this.cdRef.markForCheck();
   }
 }
