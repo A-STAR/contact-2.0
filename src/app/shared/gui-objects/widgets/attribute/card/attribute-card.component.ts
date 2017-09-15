fd01fbd847d96d5caf12960614a2fce9bfa182cb
Dmitry Maltsev <d.maltsev@crif.com>
2017-09-15 13:35:50 +0300

    feat: attribute card boilerplate
    #refs WBE20-285

---
+++ src/app/shared/gui-objects/widgets/attribute/card/attribute-card.component.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-attribute-card',
+  templateUrl: './attribute-card.component.html'
+})
+export class AttributeCardComponent {
+
+}
--- src/app/shared/gui-objects/widgets/attribute/grid/attribute-grid.component.ts
+++ src/app/shared/gui-objects/widgets/attribute/grid/attribute-grid.component.ts
@@ -5,27 +5,54 @@ import 'rxjs/add/operator/distinctUntilChanged';
 import { IAttribute, IAttributeResponse } from '../attribute.interface';
 import { IGridTreeColumn, IGridTreeRow } from '../../../../components/gridtree/gridtree.interface';
 import { IOption } from '../../../../../core/converter/value-converter.interface';
+import { IToolbarItem, ToolbarItemTypeEnum } from '../../../../components/toolbar-2/toolbar-2.interface';
 
 import { AttributeService } from '../attribute.service';
 import { UserDictionariesService } from '../../../../../core/user/dictionaries/user-dictionaries.service';
+
+import { DialogFunctions } from '../../../../../core/dialog';
 
 @Component({
   selector: 'app-attribute-grid',
   templateUrl: './attribute-grid.component.html',
   changeDetection: ChangeDetectionStrategy.OnPush
 })
-export class AttributeGridComponent {
+export class AttributeGridComponent extends DialogFunctions {
   private _columns: Array<IGridTreeColumn<IAttribute>> = [
-    { label: 'Id', prop: 'id' },
     { label: 'Name', prop: 'name' },
     { label: 'Code', prop: 'code' },
+    { label: 'TypeCode', prop: 'typeCode' },
   ];
+
+  toolbarItems: Array<IToolbarItem> = [
+    {
+      type: ToolbarItemTypeEnum.BUTTON_ADD,
+      action: () => console.log('add'),
+    },
+    {
+      type: ToolbarItemTypeEnum.BUTTON_EDIT,
+      action: () => console.log('edit'),
+    },
+    {
+      type: ToolbarItemTypeEnum.BUTTON_DELETE,
+      action: () => console.log('delete'),
+    },
+    {
+      type: ToolbarItemTypeEnum.BUTTON_REFRESH,
+      action: () => console.log('fetch'),
+    },
+  ];
+
+  dialog: 'add' | 'edit' | 'delete';
+
+  rows: IGridTreeRow<Partial<IAttribute>>[] = [];
 
   constructor(
     private attributeService: AttributeService,
     private cdRef: ChangeDetectorRef,
     private userDictionariesService: UserDictionariesService,
   ) {
+    super();
     this.fetch(1);
   }
 
@@ -38,8 +65,6 @@ export class AttributeGridComponent {
   get columns(): Array<IGridTreeColumn<IAttribute>> {
     return this._columns;
   }
-
-  rows: IGridTreeRow<Partial<IAttribute>>[] = [];
 
   onChange(event: Event): void {
     this.fetch(Number((event.target as HTMLSelectElement).value));
