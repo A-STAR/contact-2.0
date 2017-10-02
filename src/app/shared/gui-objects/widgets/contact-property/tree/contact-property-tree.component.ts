b1c45871d8274a7e62d201a7210ca072d1fe7797
Dmitry Maltsev <d.maltsev@crif.com>
2017-10-02 15:04:53 +0300

    fix: tree node converting
    #refs WEB20-306

--- src/app/shared/gui-objects/widgets/contact-property/tree/contact-property-tree.component.ts
+++ src/app/shared/gui-objects/widgets/contact-property/tree/contact-property-tree.component.ts
@@ -82,27 +82,44 @@ export class ContactPropertyTreeComponent implements OnInit {
 
   private fetch(): void {
     this.contactPropertyService.fetchAll(this.contactType, this.treeType).subscribe(nodes => {
-      this._nodes = this.convertToTreeNodes(nodes);
+      const root = { id: 0 };
+      this._nodes = this.addParents([
+        {
+          ...root,
+          children: this.convertToTreeNodes(nodes)
+        }
+      ]);
       this.cdRef.markForCheck();
     });
   }
 
-  private convertToTreeNodes(nodes: IContactTreeNode[], parent: number = null): ITreeNode[] {
+  private convertToTreeNodes(nodes: IContactTreeNode[]): ITreeNode[] {
     return nodes
       .sort((a, b) => a.sortOrder - b.sortOrder)
       .map(node => {
         const { children, sortOrder, ...data } = node;
         return {
-          children: children && children.length ? this.convertToTreeNodes(children, data.id) : null,
           data,
-          parent: node,
+          ...(children && children.length ? { children: this.convertToTreeNodes(children) } : {}),
           sortOrder,
           label: String(node.id),
           // TODO(d.maltsev): switch back to name when there is valid data in the DB
           // label: node.name,
           bgColor: node.boxColor,
           id: node.id,
+          expanded: true,
         };
       });
+  }
+
+  private addParents(nodes: ITreeNode[], parent: ITreeNode = null): ITreeNode[] {
+    return nodes.map(node => {
+      const { children } = node;
+      return {
+        ...node,
+        ...(children && children.length ? { children: this.addParents(children, node) } : {}),
+        parent,
+      };
+    });
   }
 }
