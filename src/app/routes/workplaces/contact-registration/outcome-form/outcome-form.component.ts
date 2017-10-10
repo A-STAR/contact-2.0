862b601df7532b7c77b08ed8b0ad4f7921c6ca2a
Dmitry Maltsev <d.maltsev@crif.com>
2017-10-10 09:49:28 +0300

    feat: outcome form boilerplate
    #refs WEB20-310

--- src/app/routes/workplaces/contact-registration/outcome-form/outcome-form.component.ts
+++ src/app/routes/workplaces/contact-registration/outcome-form/outcome-form.component.ts
@@ -1,9 +1,25 @@
-import { Component } from '@angular/core';
+import { Component, ViewChild } from '@angular/core';
+
+import { IDynamicFormControl } from '../../../../shared/components/form/dynamic-form/dynamic-form.interface';
+
+import { DynamicFormComponent } from '../../../../shared/components/form/dynamic-form/dynamic-form.component';
+
+import { makeKey } from '../../../../core/utils';
+
+const labelKey = makeKey('modules.contactRegistration.outcomeForm')
 
 @Component({
   selector: 'app-outcome-form',
   templateUrl: './outcome-form.component.html',
 })
 export class OutcomeFormComponent {
+  @ViewChild(DynamicFormComponent) form: DynamicFormComponent;
 
+  controls: IDynamicFormControl[] = [
+    { label: labelKey('template'), controlName: 'template', type: 'textarea', disabled: true },
+    { label: labelKey('autoCommentId'), controlName: 'autoCommentId', type: 'select', options: [] },
+    { label: labelKey('autoComment'), controlName: 'autoComment', type: 'textarea', disabled: true },
+    { label: labelKey('comment'), controlName: 'comment', type: 'textarea' },
+  ];
+  data = {};
 }
