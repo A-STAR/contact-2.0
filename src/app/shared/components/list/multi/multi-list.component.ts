8d96e6b8cd0d2ae5cb99c748a8d03beaca0be6f5
Dmitry Maltsev <d.maltsev@crif.com>
2017-10-16 17:05:43 +0300

    chore: lists refactoring
    #refs WEB20-306

---
+++ src/app/shared/components/list/multi/multi-list.component.ts
@@ -0,0 +1,65 @@
+import { ChangeDetectionStrategy, ChangeDetectorRef, Component, EventEmitter, Input, Output } from '@angular/core';
+
+@Component({
+  selector: 'app-multi-list',
+  templateUrl: './multi-list.component.html',
+  styleUrls: [ './multi-list.component.scss' ],
+  changeDetection: ChangeDetectionStrategy.OnPush
+})
+export class MultiListComponent<T> {
+  @Input() items: T[];
+  @Input() height = 120;
+  @Input() hideSearchBar = false;
+  @Output() doubleClick = new EventEmitter<T>();
+  @Output() select = new EventEmitter<T>();
+
+  private _search = '';
+  private _selectedId: string = null;
+
+  constructor(private cdRef: ChangeDetectorRef) {}
+
+  @Input() getId: (item: T) => string = () => null;
+  @Input() getName: (item: T) => string = () => null;
+
+  get filteredItems(): T[] {
+    return (this.items || []).filter(item => this.getName(item).toLowerCase().includes(this._search));
+  }
+
+  get search(): string {
+    return this._search;
+  }
+
+  get style(): object {
+    return {
+      maxHeight: `${this.height}px`,
+      overflow: 'auto'
+    };
+  }
+
+  getItemClass(item: T): object {
+    return {
+      'list-group-item p-sm b0': true,
+      'active': this.getId(item) === this._selectedId
+    };
+  }
+
+  onSearch(value: string): void {
+    this._search = value.toLowerCase();
+    this.cdRef.markForCheck();
+  }
+
+  onClick(item: T): void {
+    this._selectedId = this.getId(item);
+    this.select.emit(item);
+    this.cdRef.markForCheck();
+  }
+
+  onDblClick(item: T): void {
+    this.doubleClick.emit(item);
+    this.cdRef.markForCheck();
+  }
+
+  trackByFn(index: number): number {
+    return index;
+  }
+}
