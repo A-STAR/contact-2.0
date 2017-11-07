6b29a90655ca5717e4257da3d0ae4e0d178b330f
Dmitry Maltsev <d.maltsev@crif.com>
2017-11-07 14:15:54 +0300

    feat: debtor card boilerplate
    #refs WEB20-391

--- src/app/routes/workplaces/incoming-call/debtor-card/debtor-card.component.ts
+++ src/app/routes/workplaces/incoming-call/debtor-card/debtor-card.component.ts
@@ -1,10 +1,24 @@
 import { ChangeDetectionStrategy, Component } from '@angular/core';
 
+import { makeKey } from '../../../../core/utils';
+
+const labelKey = makeKey('modules.incomingCall.debtorCard.form');
+
 @Component({
   selector: 'app-incoming-call-debtor-card',
   templateUrl: 'debtor-card.component.html',
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class DebtorCardComponent {
+  controls = [
+    { label: labelKey('userFullName'), controlName: 'userFullName', type: 'text' },
+    { label: labelKey('organization '), controlName: 'organization ', type: 'text' },
+    { label: labelKey('position'), controlName: 'position', type: 'text' },
+    { label: labelKey('mobPhone'), controlName: 'mobPhone', type: 'text' },
+    { label: labelKey('workPhone'), controlName: 'workPhone', type: 'text' },
+    { label: labelKey('intPhone'), controlName: 'intPhone', type: 'text' },
+    { label: labelKey('recommendation'), controlName: 'recommendation', type: 'text' },
+  ];
 
+  data = {};
 }
