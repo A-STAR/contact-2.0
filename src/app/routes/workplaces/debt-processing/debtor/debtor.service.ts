2f770ed09601895bcaf8288de09ab1e64cf85f6f
Dmitry Maltsev <d.maltsev@crif.com>
2017-11-07 18:18:32 +0300

    chore: removed unused imports

--- src/app/routes/workplaces/debt-processing/debtor/debtor.service.ts
+++ src/app/routes/workplaces/debt-processing/debtor/debtor.service.ts
@@ -14,8 +14,6 @@ import { DataService } from '../../../../core/data/data.service';
 import { NotificationsService } from '../../../../core/notifications/notifications.service';
 import { UserPermissionsService } from '../../../../core/user/permissions/user-permissions.service';
 
-import { combineLatestAnd } from '../../../../core/utils/helpers';
-
 @Injectable()
 export class DebtorService {
   private _debt$ = new BehaviorSubject<IDebt>(null);
