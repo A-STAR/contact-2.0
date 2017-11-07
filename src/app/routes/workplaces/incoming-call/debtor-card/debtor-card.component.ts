a66b05f509c3895c1387de505afefb409a2b0fb1
Dmitry Maltsev <d.maltsev@crif.com>
2017-11-07 17:23:45 +0300

    fix: label i18n
    #refs WEB20-391

--- src/app/routes/workplaces/incoming-call/debtor-card/debtor-card.component.ts
+++ src/app/routes/workplaces/incoming-call/debtor-card/debtor-card.component.ts
@@ -12,7 +12,7 @@ const labelKey = makeKey('modules.incomingCall.debtorCard.form');
 export class DebtorCardComponent {
   controls = [
     { label: labelKey('userFullName'), controlName: 'userFullName', type: 'text' },
-    { label: labelKey('organization '), controlName: 'organization ', type: 'text' },
+    { label: labelKey('organization'), controlName: 'organization ', type: 'text' },
     { label: labelKey('position'), controlName: 'position', type: 'text' },
     { label: labelKey('mobPhone'), controlName: 'mobPhone', type: 'text' },
     { label: labelKey('workPhone'), controlName: 'workPhone', type: 'text' },
