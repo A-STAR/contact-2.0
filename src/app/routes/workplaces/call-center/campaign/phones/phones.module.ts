9cb83d37beb3ef4ce3f3f056b3a465d2c950e2d3
Dmitry Maltsev <d.maltsev@crif.com>
2017-11-13 16:44:32 +0300

    feat: phones grid boilerplate
    #refs WEB20-411

--- src/app/routes/workplaces/call-center/campaign/phones/phones.module.ts
+++ src/app/routes/workplaces/call-center/campaign/phones/phones.module.ts
@@ -1,8 +1,13 @@
 import { NgModule } from '@angular/core';
 
+import { SharedModule } from '../../../../../shared/shared.module';
+
 import { PhonesComponent } from './phones.component';
 
 @NgModule({
+  imports: [
+    SharedModule,
+  ],
   exports: [
     PhonesComponent,
   ],
