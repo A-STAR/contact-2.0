d58b5fec08ea8f990a5a52d4b822e233dd87c800
Dmitry Maltsev <d.maltsev@crif.com>
2017-11-13 11:32:27 +0300

    feat: call center service
    #refs WEB20-411

--- src/app/routes/workplaces/call-center/call-center.component.ts
+++ src/app/routes/workplaces/call-center/call-center.component.ts
@@ -1,10 +1,24 @@
 import { ChangeDetectionStrategy, Component } from '@angular/core';
+import { Observable } from 'rxjs/Observable';
+
+import { CallCenterService } from './call-center.service';
 
 @Component({
   selector: 'app-call-center',
   templateUrl: 'call-center.component.html',
   changeDetection: ChangeDetectionStrategy.OnPush,
+  providers: [
+    CallCenterService,
+  ]
 })
 export class CallCenterComponent {
   static COMPONENT_NAME = 'CallCenterComponent';
+
+  constructor(
+    private callCenterService: CallCenterService,
+  ) {}
+
+  get shouldSelectCampaign$(): Observable<boolean> {
+    return this.callCenterService.shouldSelectCampaign$;
+  }
 }
---
+++ src/app/routes/workplaces/call-center/call-center.service.ts
@@ -0,0 +1,21 @@
+import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs/Observable';
+
+import { DataService } from '../../../core/data/data.service';
+import { NotificationsService } from '../../../core/notifications/notifications.service';
+
+import { invert, isEmpty } from '../../../core/utils';
+
+@Injectable()
+export class CallCenterService {
+  constructor(
+    private dataService: DataService,
+    private notificationsService: NotificationsService,
+  ) {}
+
+  get shouldSelectCampaign$(): Observable<boolean> {
+    return this.dataService.readAll('/userCampaigns')
+      .map(isEmpty)
+      .map(invert);
+  }
+}
