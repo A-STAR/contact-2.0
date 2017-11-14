203c64e22787d68a4b95cd4b27644253a16b4dc9
Dmitry Maltsev <d.maltsev@crif.com>
2017-11-14 18:03:27 +0300

    feat: campaign service boilerplate
    #refs WEB20-411

--- src/app/routes/workplaces/call-center/campaign/campaign.service.ts
+++ src/app/routes/workplaces/call-center/campaign/campaign.service.ts
@@ -1,6 +1,22 @@
 import { Injectable } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { BehaviorSubject } from 'rxjs/BehaviorSubject';
+
+interface ICampaignRouteParams {
+  campaignId: number;
+}
 
 @Injectable()
 export class CampaignService {
+  constructor(
+    private route: ActivatedRoute,
+  ) {}
 
+  get campaignId(): number {
+    return this.routeParams.campaignId;
+  }
+
+  get routeParams(): ICampaignRouteParams {
+    return (this.route.params as BehaviorSubject<ICampaignRouteParams>).value;
+  }
 }
