a18b7f8b8c07993a8626296ba1fd4172c61b7fb1
Dmitry Maltsev <d.maltsev@crif.com>
2017-11-14 17:55:09 +0300

    feat: debt components grid boilerplate
    #refs WEB20-411

--- src/app/routes/workplaces/call-center/campaign/debt-components/debt-components.module.ts
+++ src/app/routes/workplaces/call-center/campaign/debt-components/debt-components.module.ts
@@ -1,8 +1,13 @@
 import { NgModule } from '@angular/core';
 
+import { SharedModule } from '../../../../../shared/shared.module';
+
 import { DebtComponentsComponent } from './debt-components.component';
 
 @NgModule({
+  imports: [
+    SharedModule,
+  ],
   exports: [
     DebtComponentsComponent,
   ],
