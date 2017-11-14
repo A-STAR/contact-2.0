8061850728abc2d13c21bfd4a18a06e3813af7ce
Dmitry Maltsev <d.maltsev@crif.com>
2017-11-14 17:50:11 +0300

    feat: contacts, documents, promises, payments boilerplate
    #refs WEB20-411

--- src/app/routes/workplaces/call-center/campaign/documents/documents.module.ts
+++ src/app/routes/workplaces/call-center/campaign/documents/documents.module.ts
@@ -1,8 +1,13 @@
 import { NgModule } from '@angular/core';
 
+import { SharedModule } from '../../../../../shared/shared.module';
+
 import { DocumentsComponent } from './documents.component';
 
 @NgModule({
+  imports: [
+    SharedModule,
+  ],
   exports: [
     DocumentsComponent,
   ],
