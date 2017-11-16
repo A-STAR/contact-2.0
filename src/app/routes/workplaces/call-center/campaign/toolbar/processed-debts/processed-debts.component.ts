275e2db82f82925eecddfa819a871b8e64ebe062
Dmitry Maltsev <d.maltsev@crif.com>
2017-11-16 18:30:21 +0300

    feat: processed debts boilerplate
    #refs WEB20-411

---
+++ src/app/routes/workplaces/call-center/campaign/toolbar/processed-debts/processed-debts.component.ts
@@ -0,0 +1,10 @@
+import { ChangeDetectionStrategy, Component } from '@angular/core';
+
+@Component({
+  selector: 'app-call-center-toolbar-processed-debts',
+  templateUrl: 'processed-debts.component.html',
+  changeDetection: ChangeDetectionStrategy.OnPush,
+})
+export class ProcessedDebtsComponent {
+
+}
--- src/app/routes/workplaces/call-center/campaign/toolbar/toolbar.module.ts
+++ src/app/routes/workplaces/call-center/campaign/toolbar/toolbar.module.ts
@@ -1,11 +1,13 @@
 import { NgModule } from '@angular/core';
 
+import { ProcessedDebtsModule } from './processed-debts/processed-debts.module';
 import { SharedModule } from '../../../../../shared/shared.module';
 
 import { ToolbarComponent } from './toolbar.component';
 
 @NgModule({
   imports: [
+    ProcessedDebtsModule,
     SharedModule,
   ],
   exports: [
