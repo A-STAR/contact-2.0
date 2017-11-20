af9534d8bffc09ec70400fd33d19af88679cf481
Dmitry Maltsev <d.maltsev@crif.com>
2017-11-20 11:46:13 +0300

    feat: debts processing tabs
    #refs WEB20-400

--- src/app/routes/workplaces/debt-processing/debt-processing.component.ts
+++ src/app/routes/workplaces/debt-processing/debt-processing.component.ts
@@ -1,4 +1,8 @@
-import { ChangeDetectorRef, ChangeDetectionStrategy, Component } from '@angular/core';
+import { ChangeDetectionStrategy, Component } from '@angular/core';
+
+import { makeKey } from '../../../core/utils';
+
+const labelKey = makeKey('modules.debtsProcessing');
 
 @Component({
   selector: 'app-debt-processing',
@@ -7,4 +11,12 @@ import { ChangeDetectorRef, ChangeDetectionStrategy, Component } from '@angular/
 })
 export class DebtProcessingComponent {
   static COMPONENT_NAME = 'DebtProcessingComponent';
+
+  grids = [
+    { key: 'debtsprocessingall', title: labelKey('all.title') },
+    // TODO(d.maltsev): uncomment when the API is done.
+    // TODO(d.maltsev): better server error handling
+    // { key: 'debtsprocessingcallback', title: labelKey('callBack.title') },
+    // { key: 'debtsprocessingcurrentjob', title: labelKey('currentJob.title') },
+  ];
 }
