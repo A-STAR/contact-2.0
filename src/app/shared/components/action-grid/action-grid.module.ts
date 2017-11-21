9bb6b1b11471cad06b314bc8313d974ba3b83ee7
Igor Kibisov <donar061@gmail.com>
2017-11-21 10:43:48 +0300

    feat: promise confirm action
    #refs WEB20-371

--- src/app/shared/components/action-grid/action-grid.module.ts
+++ src/app/shared/components/action-grid/action-grid.module.ts
@@ -4,6 +4,7 @@ import { CommonModule } from '@angular/common';
 import { ContactLogModule } from '../../gui-objects/widgets/contact-log/contact-log.module';
 import { EntityGroupModule } from '../../gui-objects/widgets/entity-group/entity-group.module';
 import { DebtResponsibleModule } from '../../gui-objects/widgets/debt-responsible/debt-responsible.module';
+import { PromiseResolveModule } from '../../gui-objects/widgets/promise-resolve/promise-resolve.module';
 import { Grid2Module } from '../grid2/grid2.module';
 
 import { ActionGridComponent } from './action-grid.component';
@@ -13,6 +14,7 @@ import { ActionGridComponent } from './action-grid.component';
     CommonModule,
     ContactLogModule,
     DebtResponsibleModule,
+    PromiseResolveModule,
     EntityGroupModule,
     Grid2Module,
   ],
