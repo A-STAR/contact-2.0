decd389b155b24ad74c92a2c34f0acd9b12b6b89
Dmitry Maltsev <d.maltsev@crif.com>
2017-11-24 13:18:54 +0300

    chore: call center api
    #refs WEB20-411

--- src/app/routes/workplaces/call-center/call-center.service.ts
+++ src/app/routes/workplaces/call-center/call-center.service.ts
@@ -6,8 +6,8 @@ import { ICampaign } from './call-center.interface';
 
 import { ContentTabService } from '../../../shared/components/content-tabstrip/tab/content-tab.service';
 
-// import { DataService } from '../../../core/data/data.service';
-// import { NotificationsService } from '../../../core/notifications/notifications.service';
+import { DataService } from '../../../core/data/data.service';
+import { NotificationsService } from '../../../core/notifications/notifications.service';
 
 @Injectable()
 export class CallCenterService {
@@ -15,8 +15,8 @@ export class CallCenterService {
 
   constructor(
     private contentTabService: ContentTabService,
-    // private dataService: DataService,
-    // private notificationsService: NotificationsService,
+    private dataService: DataService,
+    private notificationsService: NotificationsService,
   ) {
     this.fetchCampaigns().subscribe(campaigns => {
       this._campaigns$.next(campaigns);
@@ -35,13 +35,7 @@ export class CallCenterService {
   }
 
   private fetchCampaigns(): Observable<ICampaign[]> {
-    return Observable.of([
-      { id: 1, name: 'Campaign 1', comment: '' },
-      { id: 2, name: 'Campaign 2', comment: '' },
-    ]);
-
-    // TODO(d.maltsev): uncomment when API is ready
-    // return this.dataService.readAll('/userCampaigns')
-    //   .catch(this.notificationsService.fetchError().entity('entities.campaign.gen.plural').dispatchCallback());
+    return this.dataService.readAll('/userCampaigns')
+      .catch(this.notificationsService.fetchError().entity('entities.campaign.gen.plural').dispatchCallback());
   }
 }
