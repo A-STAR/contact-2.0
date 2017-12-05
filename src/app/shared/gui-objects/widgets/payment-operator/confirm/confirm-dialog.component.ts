69ec36d5969e3788d78efbf3ab51be3e27b5edfd
Mikhail Bobryshev <M.Bobryshev@crif.com>
2017-12-05 09:09:37 +0300

    ref payment operator

--- src/app/shared/gui-objects/widgets/payment-operator/confirm/confirm-dialog.component.ts
+++ src/app/shared/gui-objects/widgets/payment-operator/confirm/confirm-dialog.component.ts
@@ -1,33 +1,24 @@
-import { ChangeDetectionStrategy, Component, EventEmitter, Output, Input, OnInit } from '@angular/core';
+import { ChangeDetectionStrategy, Component, EventEmitter, Output, Input } from '@angular/core';
 
 import { ICloseAction } from '../../../../components/action-grid/action-grid.interface';
 
 import { PaymentOperatorService } from '../payment-operator.service';
 
-import { DialogFunctions } from '../../../../../core/dialog';
 
 @Component({
   selector: 'app-payment-operator-confirm-dialog',
   templateUrl: './confirm-dialog.component.html',
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
-export class OperatorConfirmDialogComponent extends DialogFunctions implements OnInit {
+export class OperatorConfirmDialogComponent  {
   @Input() payments: number[];
 
   @Output() close = new EventEmitter<ICloseAction>();
 
-  dialog: string = null;
-  count: number;
+  constructor(private paymentOperatorService: PaymentOperatorService) {}
 
-  constructor(private paymentOperatorService: PaymentOperatorService) {
-    super();
-  }
+    onConfirm(): void {
 
-  ngOnInit(): void {
-  }
-
-  onConfirm(): void {
-    this.setDialog();
     this.paymentOperatorService.confirm(this.payments)
       .subscribe(res => {
         const refresh = !!res.massInfo && !!res.massInfo.total;
--- src/app/shared/gui-objects/widgets/payment-operator/reject/reject-dialog.component.ts
+++ src/app/shared/gui-objects/widgets/payment-operator/reject/reject-dialog.component.ts
@@ -1,33 +1,22 @@
-import { ChangeDetectionStrategy, Component, EventEmitter, Output, Input, OnInit } from '@angular/core';
+import { ChangeDetectionStrategy, Component, EventEmitter, Output, Input } from '@angular/core';
 
 import { ICloseAction } from '../../../../components/action-grid/action-grid.interface';
 
 import { PaymentOperatorService } from '../payment-operator.service';
-
-import { DialogFunctions } from '../../../../../core/dialog';
 
 @Component({
   selector: 'app-payment-operator-reject-dialog',
   templateUrl: './reject-dialog.component.html',
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
-export class OperatorRejectDialogComponent extends DialogFunctions implements OnInit {
+export class OperatorRejectDialogComponent  {
   @Input() payments: number[];
 
   @Output() close = new EventEmitter<ICloseAction>();
 
-  dialog: string = null;
-  count: number;
-
-  constructor(private paymentOperatorService: PaymentOperatorService) {
-    super();
-  }
-
-  ngOnInit(): void {
-  }
+  constructor(private paymentOperatorService: PaymentOperatorService) {}
 
   onReject(): void {
-    this.setDialog();
     this.paymentOperatorService.reject(this.payments)
       .subscribe(res => {
         const refresh = !!res.massInfo && !!res.massInfo.total;
