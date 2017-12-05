99c3f804328f02b1c7f88d560be23478ed01c5f2
Mikhail Bobryshev <M.Bobryshev@crif.com>
2017-12-05 11:07:22 +0300

    ref, type fix

--- src/app/shared/gui-objects/widgets/promise-resolve/dialog/remove/promise-remove-dialog.component.ts
+++ src/app/shared/gui-objects/widgets/promise-resolve/dialog/remove/promise-remove-dialog.component.ts
@@ -1,4 +1,4 @@
-import { ChangeDetectionStrategy, ChangeDetectorRef, Component, EventEmitter, Output, Input, OnInit } from '@angular/core';
+import { ChangeDetectionStrategy, ChangeDetectorRef, Component, EventEmitter, Output, Input } from '@angular/core';
 
 import { PromiseResolveService } from '../../promise-resolve.service';
 import { ICloseAction } from '../../../../../components/action-grid/action-grid.interface';
@@ -8,23 +8,19 @@ import { ICloseAction } from '../../../../../components/action-grid/action-grid.
   templateUrl: './promise-remove-dialog.component.html',
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
-export class PromiseRemoveDialogComponent  implements OnInit {
+export class PromiseRemoveDialogComponent {
 
   @Input() promises: number[];
 
   @Output() close = new EventEmitter<ICloseAction>();
 
-  count: number;
-  successCount: number;
 
   constructor(
     private cdRef: ChangeDetectorRef,
     private promiseResolveService: PromiseResolveService,
   ) {}
 
-  ngOnInit(): void {
 
-  }
 
   onConfirm(): void {
     this.promiseResolveService.remove(this.promises)
