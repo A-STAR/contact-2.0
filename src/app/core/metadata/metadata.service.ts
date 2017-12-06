5801fee1e7672f75ebb56563b118bf6ecc46f0e7
Dmitry Maltsev <d.maltsev@crif.com>
2017-12-06 17:27:34 +0300

    refactoring: metadata

--- src/app/core/metadata/metadata.service.ts
+++ src/app/core/metadata/metadata.service.ts
@@ -19,14 +19,6 @@ export class MetadataService {
     this.state$.subscribe(state => this.state = state);
   }
 
-  getMetadata(key: string): Observable<IMetadataColumn[]> {
-    return this.getData(key, 'columns');
-  }
-
-  getActions(key: string): Observable<IMetadataAction[]> {
-    return this.getData(key, 'actions');
-  }
-
   refresh(key: string): void {
     this.store.dispatch({
       type: MetadataService.METADATA_FETCH,
@@ -34,15 +26,14 @@ export class MetadataService {
     });
   }
 
-  private getData(metadataKey: string, key: 'actions' | 'columns'): Observable<any[]> {
-    const status = this.state[metadataKey] && this.state[metadataKey].status;
+  getData(key: string): Observable<any> {
+    const status = this.state[key] && this.state[key].status;
     if (!status || status === MetadataListStatusEnum.ERROR) {
-      this.refresh(metadataKey);
+      this.refresh(key);
     }
     return this.state$
-      .map(state => state[metadataKey])
-      .filter(list => list && list.status === MetadataListStatusEnum.LOADED)
-      .map(list => list[key] || []);
+      .map(state => state[key])
+      .filter(list => list && list.status === MetadataListStatusEnum.LOADED);
   }
 
   private get state$(): Observable<IMetadataState> {
