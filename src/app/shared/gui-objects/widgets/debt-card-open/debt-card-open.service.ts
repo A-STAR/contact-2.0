c3107a4f0100592224826eb8a60ed19e346357f1
Mikhail Bobryshev <M.Bobryshev@crif.com>
2017-12-07 12:00:35 +0300

    fix error message

--- src/app/shared/gui-objects/widgets/debt-card-open/debt-card-open.service.ts
+++ src/app/shared/gui-objects/widgets/debt-card-open/debt-card-open.service.ts
@@ -16,6 +16,6 @@ export class OpenDebtCardService {
   getFirstDebtsByUserId(personsId: number): Observable<any> {
     return this.dataService.read(this.url, { personsId } )
       .map(res => res && res.id)
-      .catch(this.notificationsService.deleteError().entity('entities.sms.debts.plural').dispatchCallback());
+      .catch(this.notificationsService.deleteError().entity('entities.debts.gen.plural').dispatchCallback());
    }
 }
