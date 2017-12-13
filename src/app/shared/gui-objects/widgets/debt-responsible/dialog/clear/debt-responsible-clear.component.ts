205445a2fa6323560b1fd22a80484acb774a4aee
Igor Kibisov <donar061@gmail.com>
2017-12-13 17:06:43 +0300

    ref: debt responsible result notification
    #refs WEB20-509

--- src/app/shared/gui-objects/widgets/debt-responsible/dialog/clear/debt-responsible-clear.component.ts
+++ src/app/shared/gui-objects/widgets/debt-responsible/dialog/clear/debt-responsible-clear.component.ts
@@ -1,38 +1,32 @@
-import { ChangeDetectionStrategy, ChangeDetectorRef, Component, EventEmitter, Output, Input, OnInit } from '@angular/core';
+import { ChangeDetectionStrategy, Component, EventEmitter, Output, Input, OnInit } from '@angular/core';
+
+import { ICloseAction } from '../../../../../components/action-grid/action-grid.interface';
+import { IOperationResult } from '../../debt-responsible.interface';
 
 import { DebtResponsibleService } from '../../debt-responsible.service';
 
-import { DialogFunctions } from '../../../../../../core/dialog';
-
 @Component({
   selector: 'app-debt-responsible-clear',
-  templateUrl: './debt-responsible-clear.component.html',
+  template: '',
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
-export class DebtResponsibleClearComponent extends DialogFunctions implements OnInit {
+export class DebtResponsibleClearComponent implements OnInit {
 
   @Input() debts: number[];
-  @Output() close = new EventEmitter<null>();
-
-  dialog: string = null;
-  count: number;
-  successCount: number;
+  @Output() close = new EventEmitter<ICloseAction>();
 
   constructor(
-    private cdRef: ChangeDetectorRef,
     private debtResponsibleService: DebtResponsibleService
-  ) {
-    super();
-  }
+  ) { }
 
   ngOnInit(): void {
     this.debtResponsibleService.clearResponsible(this.debts)
-      .subscribe(result => {
-        this.count = result.massInfo.total;
-        this.successCount = result.massInfo.processed;
-        this.setDialog('clearResult');
-        this.cdRef.markForCheck();
-      });
+      .subscribe(result => this.onOperationResult(result));
+  }
+
+  onOperationResult(result: IOperationResult): void {
+    this.debtResponsibleService.showOperationNotification(result);
+    this.close.emit({ refresh: result.massInfo && !!result.massInfo.processed });
   }
 
   onClose(): void {
