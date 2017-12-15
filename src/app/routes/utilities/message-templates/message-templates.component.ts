7108914ba513539111a83dfd86450067f437ef5d
Dmitri Maltsev <dmitri.maltsev@hotmail.com>
2017-12-15 12:05:09 +0300

    feat: new tabview
    #refs WEB20-497

--- src/app/routes/utilities/message-templates/message-templates.component.ts
+++ src/app/routes/utilities/message-templates/message-templates.component.ts
@@ -7,4 +7,20 @@ import { ChangeDetectionStrategy, Component } from '@angular/core';
 })
 export class MessageTemplatesComponent {
   static COMPONENT_NAME = 'MessageTemplatesComponent';
+
+  tabs = [
+    { isInitialised: true,  title: 'utilities.messageTemplates.phoneCall.title', typeCode: 1 },
+    { isInitialised: false, title: 'utilities.messageTemplates.sms.title', typeCode: 2 },
+    { isInitialised: false, title: 'utilities.messageTemplates.email.title', typeCode: 3 },
+    { isInitialised: false, title: 'utilities.messageTemplates.autoComment.title', typeCode: 4 },
+    { isInitialised: false, title: 'utilities.messageTemplates.custom.title', typeCode: 5 },
+  ];
+
+  shouldDisplayTab(tabIndex: number): boolean {
+    return this.tabs[tabIndex].isInitialised;
+  }
+
+  onTabSelect(tabIndex: number): void {
+    this.tabs[tabIndex].isInitialised = true;
+  }
 }
