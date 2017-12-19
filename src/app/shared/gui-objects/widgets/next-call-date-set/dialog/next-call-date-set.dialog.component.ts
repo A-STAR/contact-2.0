7289a591fb38d13c37890c98f560aad8a1de03f3
mbob72 <mbob72@gmail.com>
2017-12-19 21:23:20 +0300

    module done without dictionary and styles

--- src/app/shared/gui-objects/widgets/next-call-date-set/dialog/next-call-date-set.dialog.component.ts
+++ src/app/shared/gui-objects/widgets/next-call-date-set/dialog/next-call-date-set.dialog.component.ts
@@ -1,9 +1,16 @@
-import { ChangeDetectionStrategy, ChangeDetectorRef, Component, EventEmitter, Output, Input, Inject } from '@angular/core';
+import { ChangeDetectionStrategy, ChangeDetectorRef, Component,
+  EventEmitter, Output, Input, Inject, ViewChild } from '@angular/core';
 
 import { ICloseAction } from '../../../../components/action-grid/action-grid.interface';
+import { IDynamicFormControl } from '../../../../components/form/dynamic-form/dynamic-form.interface';
+import { DynamicFormComponent } from '../../../../components/form/dynamic-form/dynamic-form.component';
+import * as moment from 'moment';
 // import { IEntityGroup } from '../../../entity-group/entity-group.interface';
 
 import { NextCallDateSetService } from '../next-call-date-set.service';
+import { makeKey } from '../../../../../core/utils';
+
+const labelKey = makeKey('widgets.actionLog.form');
 
 @Component({
   selector: 'app-next-call-date-set',
@@ -11,6 +18,7 @@ import { NextCallDateSetService } from '../next-call-date-set.service';
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class NextCallDateSetDialogComponent  {
+  @ViewChild(DynamicFormComponent) form: DynamicFormComponent;
   @Input() debts: number[];
 
   @Output() close = new EventEmitter<ICloseAction>();
@@ -20,9 +28,26 @@ export class NextCallDateSetDialogComponent  {
     private nextCallDateSetService: NextCallDateSetService,
   ) {}
 
+  controls: IDynamicFormControl[] = [
+    { label: labelKey('startDate'), controlName: 'nextCallDate', type: 'datepicker',
+      displayTime: true, minDate:  moment().toDate(), width: 5 }
+  ];
+
+  get canSubmit(): boolean {
+    return this.form.canSubmit;
+  }
+
+  onSubmit(): void {
+    const typeCode = this.form.getControl('nextCallDate').value;
+    const data = Object.assign({}, this.form.serializedUpdates, { typeCode });
+    this.close.emit();
+    this.nextCallDateSetService.setNextCall(this.debts, data.nextCallDate)
+      .subscribe(() => ({}));
+  }
+
+
 
   onClose(): void {
-    console.log(this.debts);
     this.close.emit();
   }
 }
--- src/app/shared/gui-objects/widgets/next-call-date-set/next-call-date-set.service.ts
+++ src/app/shared/gui-objects/widgets/next-call-date-set/next-call-date-set.service.ts
@@ -8,8 +8,29 @@ import { IAppState } from '../../../../core/state/state.interface';
 
 import { AbstractActionService } from '../../../../core/state/action.service';
 import { DataService } from '../../../../core/data/data.service';
+import { NotificationsService } from '../../../../core/notifications/notifications.service';
 
 @Injectable()
 export class  NextCallDateSetService {
+  constructor(
+    private dataService: DataService,
+    private notificationsService: NotificationsService,
+  ) {}
+
+  private url = '/mass/debts/nextCall';
+
+  setNextCall(debts: number[], nextCallDateTime: string): Observable<any> {
+    const ids = debts.map(id => [ id ]);
+    return this.dataService.update(this.url, {}, { idData: { ids }, actionData: { nextCallDateTime }} )
+      // dictionary
+      .do(res => {
+        if (!res.success) {
+          this.notificationsService.warning().entity('default.dialog.result.messageUnsuccessful').response(res).dispatch();
+        } else {
+          this.notificationsService.info().entity('default.dialog.result.message').response(res).dispatch();
+        }
+      })
+      .catch(this.notificationsService.updateError().entity('entities.managers.gen.singular').dispatchCallback());
+  }
 
 }
