111a34946cab3111e61baed165317d8c12dcb282
Ivan Lobanov <arkhemlol@gmail.com>
2017-12-20 18:54:42 +0300

    fix: imports
    #refs WEB20-367

--- src/app/shared/gui-objects/widgets/debt-status/debt-status.module.ts
+++ src/app/shared/gui-objects/widgets/debt-status/debt-status.module.ts
@@ -1,5 +1,6 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { DialogModule } from '../../../../shared/components/dialog/dialog.module';
 
 import { DebtStatusService } from './debt-status.service';
 
@@ -7,9 +8,11 @@ import { DebtStatusComponent } from './dialog/debt-status.component';
 
 @NgModule({
   imports: [
-    CommonModule
+    CommonModule,
+    DialogModule
   ],
   providers: [DebtStatusService],
-  declarations: [DebtStatusComponent]
+  declarations: [DebtStatusComponent],
+  exports: [DebtStatusComponent]
 })
 export class DebtStatusModule { }
