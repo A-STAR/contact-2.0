256d0d3186bda624fbe2eef5088c2641e94b5425
mbob72 <mbob72@gmail.com>
2017-12-20 13:29:25 +0300

    comments remove

--- src/app/shared/gui-objects/widgets/next-call-date-set/next-call-date-set.module.ts
+++ src/app/shared/gui-objects/widgets/next-call-date-set/next-call-date-set.module.ts
@@ -2,14 +2,9 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { TranslateModule } from '@ngx-translate/core';
 
-
-// import { DialogActionModule } from '../../../components/dialog-action/dialog-action.module';
 import { DialogModule } from '../../../components/dialog/dialog.module';
 import { DynamicFormModule } from '../../../components/form/dynamic-form/dynamic-form.module';
 import { NextCallDateSetDialogComponent } from './next-call-date-set.component';
-
-// import { NextCallDateSetDialogModule } from './dialog/next-call-date-set.dialog.module';
-
 
 import { NextCallDateSetService } from './next-call-date-set.service';
 
