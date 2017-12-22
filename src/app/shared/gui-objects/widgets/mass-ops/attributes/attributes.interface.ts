13af4a30d9627e0529d50683af0189f45c62847a
Ivan Lobanov <arkhemlol@gmail.com>
2017-12-22 15:54:05 +0300

    feat: added attr service methods
    #refs WEB20-361

---
+++ src/app/shared/gui-objects/widgets/mass-ops/attributes/attributes.interface.ts
@@ -0,0 +1,13 @@
+export interface IDebtAttributeChange {
+  // TODO:(i.lobanov) clarify this
+  statusCode?: number;
+  portfolioId?: number;
+  creditTypeCode?: number;
+  dict1Code?: number;
+  dict2Code?: number;
+  dict3Code?: number;
+  dict4Code?: number;
+  regionCode?: number;
+  branchCode?: number;
+  utc?: number;
+}
--- src/app/shared/gui-objects/widgets/mass-ops/attributes/attributes.service.ts
+++ src/app/shared/gui-objects/widgets/mass-ops/attributes/attributes.service.ts
@@ -1,14 +1,37 @@
 import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs/Observable';
+
+import { IDebtAttributeChange } from './attributes.interface';
+import { ILookupPortfolio, ILookupTimeZone } from '../../../../../core/lookup/lookup.interface';
+import { IOperationResult } from '../../debt-responsible/debt-responsible.interface';
 
 import { LookupService } from '../../../../../core/lookup/lookup.service';
+import { DataService } from '../../../../../core/data/data.service';
+import { NotificationsService } from '../../../../../core/notifications/notifications.service';
 
 @Injectable()
 export class AttributesService {
 
   constructor(
-    private lookupService: LookupService
+    private dataService: DataService,
+    private lookupService: LookupService,
+    private notificationsService: NotificationsService
   ) { }
 
+  change(debts: number[], data: IDebtAttributeChange): Observable<IOperationResult> {
+    const ids = debts.map(debtId => [ debtId ]);
+    return this.dataService
+      .update('/mass/debts/statuschange', {}, { idData: { ids }, actionData: data })
+      .catch(this.notificationsService.error('errors.default.update')
+        .entity('entities.operator.gen.singular').dispatchCallback());
+  }
 
+  getPortfolios(): Observable<ILookupPortfolio[]> {
+    return this.lookupService.portfolios;
+  }
+
+  getTimezones(): Observable<ILookupTimeZone[]> {
+    return this.lookupService.timezone;
+  }
 
 }
