2f88de5f0ffb3242299bc1dcec8d45f0c7a63215
Ivan Lobanov <arkhemlol@gmail.com>
2017-12-25 13:22:34 +0300

    feat: show mass operation result notification
    #refs WEB20-361

--- src/app/shared/gui-objects/widgets/mass-ops/attributes/attributes.service.ts
+++ src/app/shared/gui-objects/widgets/mass-ops/attributes/attributes.service.ts
@@ -1,5 +1,6 @@
 import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs/Observable';
+import { catchError, tap } from 'rxjs/operators';
 
 import { IDebtAttributeChange } from './attributes.interface';
 import { ILookupPortfolio, ILookupTimeZone } from '../../../../../core/lookup/lookup.interface';
@@ -19,11 +20,18 @@ export class AttributesService {
   ) { }
 
   change(debts: number[], data: IDebtAttributeChange): Observable<IOperationResult> {
-    const ids = debts.map(debtId => [ debtId ]);
+    const ids = debts.map(debtId => [debtId]);
     return this.dataService
-      .update('/mass/debts/statuschange', {}, { idData: { ids }, actionData: data })
-      .catch(this.notificationsService.error('errors.default.update')
-        .entity('entities.operator.gen.singular').dispatchCallback());
+      .update('/mass/debts/attributechange', {}, { idData: { ids }, actionData: data })
+      .pipe(
+      tap(response => {
+        if (response.success) {
+          this.notificationsService.info().entity('default.dialog.result.message').response(response).dispatch();
+        } else {
+          this.notificationsService.warning().entity('default.dialog.result.messageUnsuccessful').response(response).dispatch();
+        }
+      }),
+      catchError(this.notificationsService.updateError().entity('entities.attribute.gen.plural').dispatchCallback()));
   }
 
   getPortfolios(): Observable<ILookupPortfolio[]> {
