98254fb25ae3807213e51cb54f7d8ecc2ef1a766
Igor Kibisov <donar061@gmail.com>
2017-12-27 16:14:03 +0300

    feat: currencies mock
    #refs WEB20-561

--- src/app/shared/gui-objects/widgets/currencies/currencies.service.ts
+++ src/app/shared/gui-objects/widgets/currencies/currencies.service.ts
@@ -9,7 +9,7 @@ import { ICurrency } from './currencies.interface';
 
 import { AbstractActionService } from '../../../../core/state/action.service';
 import { DataService } from '../../../../core/data/data.service';
-import { EntityTranslationsService } from '../../../../core/entity/translations/entity-translations.service';
+// import { EntityTranslationsService } from '../../../../core/entity/translations/entity-translations.service';
 import { NotificationsService } from '../../../../core/notifications/notifications.service';
 import { UserPermissionsService } from '../../../../core/user/permissions/user-permissions.service';
 
@@ -17,15 +17,15 @@ import { UserPermissionsService } from '../../../../core/user/permissions/user-p
 export class CurrenciesService extends AbstractActionService {
   static MESSAGE_CURRENCY_SAVED = 'MESSAGE_CURRENCY_SAVED';
 
-  static CURRENCY_NAME_ID = 162;
-  static CURRENCY_SHORT_NAME_ID = 163;
+  static CURRENCY_NAME_ID = 396;
+  static CURRENCY_SHORT_NAME_ID = 396;
 
   private baseUrl = '/currencies';
 
   constructor(
     protected actions: Actions,
     private dataService: DataService,
-    private entityTranslationsService: EntityTranslationsService,
+    // private entityTranslationsService: EntityTranslationsService,
     private notificationsService: NotificationsService,
     protected store: Store<IAppState>,
     private userPermissionsService: UserPermissionsService,
@@ -50,13 +50,18 @@ export class CurrenciesService extends AbstractActionService {
   }
 
   fetchAll(): Observable<Array<ICurrency>> {
-    return this.dataService.readAll(this.baseUrl)
-      .catch(this.notificationsService.fetchError().entity('entities.currencies.gen.plural').dispatchCallback());
+    // return this.dataService.readAll(this.baseUrl)
+      // .catch(this.notificationsService.fetchError().entity('entities.currencies.gen.plural').dispatchCallback());
+    return Observable.of([
+      { id: 1, code: 'USD', name: 'US Dollar', shortName: 'Dollar', isMain: 1 },
+      { id: 2, code: 'EUR', name: 'Euro', shortName: '', isMain: 0 }
+    ]);
   }
 
   fetch(currencyId: number): Observable<ICurrency> {
-    return this.dataService.read(`${this.baseUrl}/{currencyId}`, { currencyId })
-      .catch(this.notificationsService.fetchError().entity('entities.currencies.gen.singular').dispatchCallback());
+    // return this.dataService.read(`${this.baseUrl}/{currencyId}`, { currencyId })
+      // .catch(this.notificationsService.fetchError().entity('entities.currencies.gen.singular').dispatchCallback());
+    return Observable.of({ id: 2, code: 'EUR', name: 'Euro', shortName: '', isMain: 0 });
   }
 
   create(currency: ICurrency): Observable<ICurrency> {
@@ -75,10 +80,18 @@ export class CurrenciesService extends AbstractActionService {
   }
 
   readCurrencyNameTranslations(currencyId: number): Observable<IEntityTranslation[]> {
-    return this.entityTranslationsService.readTranslations(currencyId, CurrenciesService.CURRENCY_NAME_ID);
+    // return this.entityTranslationsService.readTranslations(currencyId, CurrenciesService.CURRENCY_NAME_ID);
+    return Observable.of([
+      { languageId: 1, value: 'Евро' },
+      { languageId: 2, value: 'Euro' }
+    ]);
   }
 
   readCurrencyShortNameTranslations(currencyId: number): Observable<IEntityTranslation[]> {
-    return this.entityTranslationsService.readTranslations(currencyId, CurrenciesService.CURRENCY_SHORT_NAME_ID);
+    // return this.entityTranslationsService.readTranslations(currencyId, CurrenciesService.CURRENCY_SHORT_NAME_ID);
+    return Observable.of([
+      { languageId: 1, value: 'Евро' },
+      { languageId: 2, value: 'Euro' }
+    ]);
   }
 }
