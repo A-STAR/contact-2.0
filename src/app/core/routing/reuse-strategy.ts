4bbf60d741cc79edefe90ebedb98380df2ef1334
Dmitri Maltsev <dmitri.maltsev@hotmail.com>
2018-01-09 09:53:19 +0300

    fix: route reuse strategy

--- src/app/core/routing/reuse-strategy.ts
+++ src/app/core/routing/reuse-strategy.ts
@@ -53,7 +53,9 @@ export class ReuseStrategy implements RouteReuseStrategy {
 
   retrieve(route: ActivatedRouteSnapshot): DetachedRouteHandle {
     const url = this.getFullRouteUrl(route);
-    return this.routeCache.get(url);
+    return this.routeCache.has(url)
+      ? this.routeCache.get(url).handle
+      : null;
   }
 
   /**
