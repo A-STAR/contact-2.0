deec1b164f5d31d0b9dcbda5ee50b5ee3e55c052
Igor Kibisov <donar061@gmail.com>
2018-01-10 10:56:25 +0300

    feat: person select card
    #refs WEB20-596

--- src/app/shared/gui-objects/widgets/person-select/person-select.module.ts
+++ src/app/shared/gui-objects/widgets/person-select/person-select.module.ts
@@ -2,12 +2,14 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
 import { PersonSelectGridModule } from './grid/person-select-grid.module';
+import { PersonSelectCardModule } from './card/person-select-card.module';
 
 import { PersonSelectService } from './person-select.service';
 
 @NgModule({
   imports: [
     PersonSelectGridModule,
+    PersonSelectCardModule,
     CommonModule,
   ],
   exports: [
