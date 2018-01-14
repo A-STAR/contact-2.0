0f802189f3d9cdf2e21b29530f4c134e27f1d0e3
Alexander Tymchuk <alexander.tymchuk@gmail.com>
2018-01-14 22:14:28 +0300

    ref: translation for group edit form

--- src/app/core/entity/translations/entity-translations.interface.ts
+++ src/app/core/entity/translations/entity-translations.interface.ts
@@ -9,6 +9,7 @@ export class EntityTranslationsConstants {
   static SPEC_TEMPLATE_ATTRIBUTE_USER_NAME = 313;
   static SPEC_ATTRIBUTE_NAME = 318;
   static SPEC_CONTACT_TREE_NAME = 343;
+  static SPEC_GROUP_NAME = 396;
   static SPEC_CAMPAIGN_NAME = 402;
 }
 
