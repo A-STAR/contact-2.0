9df221ee054b0984da0a8b63f7f102784f09c301
Dmitri Maltsev <dmitri.maltsev@hotmail.com>
2018-01-15 18:26:38 +0300

    fix: debt processing child routes

--- src/app/routes/workplaces/debt-processing/debt-processing.module.ts
+++ src/app/routes/workplaces/debt-processing/debt-processing.module.ts
@@ -7,8 +7,6 @@ import { DebtProcessingService } from './debt-processing.service';
 
 import { DebtProcessingComponent } from './debt-processing.component';
 
-const gridKeys = [ 'all', 'callBack', 'currentJob', 'visits', 'promisePay', 'partPay', 'problem', 'returned' ];
-
 const routes: Routes = [
   {
     path: '',
@@ -24,7 +22,15 @@ const routes: Routes = [
       },
       // Can't pass grid key as route param because component will always be reused.
       // Instead, we have to use separate route for every grid key.
-      ...gridKeys.map(path => ({ path, loadChildren: './grid/grid.module#GridModule' })),
+      // Also, [].map doesn't work here, so we have to list every route manually.
+      { path: 'all',        loadChildren: './grid/grid.module#GridModule' },
+      { path: 'callBack',   loadChildren: './grid/grid.module#GridModule' },
+      { path: 'currentJob', loadChildren: './grid/grid.module#GridModule' },
+      { path: 'visits',     loadChildren: './grid/grid.module#GridModule' },
+      { path: 'promisePay', loadChildren: './grid/grid.module#GridModule' },
+      { path: 'partPay',    loadChildren: './grid/grid.module#GridModule' },
+      { path: 'problem',    loadChildren: './grid/grid.module#GridModule' },
+      { path: 'returned',   loadChildren: './grid/grid.module#GridModule' },
     ],
   },
 ];
