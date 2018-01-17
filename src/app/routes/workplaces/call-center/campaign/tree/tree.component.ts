ed5ea085f71520dfea78c0697716bc45dd75183b
Dmitri Maltsev <dmitri.maltsev@hotmail.com>
2018-01-17 17:56:23 +0300

    chore: contact registration form boilerplate

--- src/app/routes/workplaces/call-center/campaign/tree/tree.component.ts
+++ src/app/routes/workplaces/call-center/campaign/tree/tree.component.ts
@@ -5,8 +5,9 @@ import { of as observableOf } from 'rxjs/observable/of';
 
 import { IDynamicFormControl } from '@app/shared/components/form/dynamic-form/dynamic-form.interface';
 
-import { CampaignService } from '../campaign.service';
-import { WorkplacesService } from '../../../workplaces.service';
+import { CampaignService } from '@app/routes/workplaces/call-center/campaign/campaign.service';
+import { UserDictionariesService } from '@app/core/user/dictionaries/user-dictionaries.service';
+import { WorkplacesService } from '@app/routes/workplaces/workplaces.service';
 
 import { isEmpty, makeKey } from '@app/core/utils';
 
@@ -25,13 +26,89 @@ export class TreeComponent implements OnInit {
   scenario: string = null;
 
   controls = [
-    { controlName: 'nextCallDateTime', type: 'datepicker', displayTime: true },
-    { controlName: 'callReasonCode', type: 'selectwrapper', dictCode: 49 },
-    { controlName: 'debtReasonCode', type: 'selectwrapper', dictCode: 11 },
-    { controlName: 'refusalReasonCode', type: 'selectwrapper', dictCode: 19, parentCode: 3 },
-    { controlName: 'statusReasonCode', type: 'selectwrapper', dictCode: 19 },
-    { controlName: 'comment', type: 'textarea' },
-  ].map(item => ({ ...item, label: labelKey(item.controlName) })) as IDynamicFormControl[];
+    // Promise
+    {
+      title: 'modules.contactRegistration.promise.title',
+      children: [
+        { controlName: 'date', type: 'datepicker', /* minDate, maxDate, */ required: true },
+        {
+          controlName: 'amount',
+          type: 'number',
+          // validators: [
+          //   minStrict(promiseMode === 2 && !canAddInsufficientAmount ? this.minDebtAmount : 0),
+          //   max(this.debt.debtAmount),
+          // ],
+          required: true,
+          // disabled: promiseMode === 3,
+          // onChange: event => this.onAmountChange(event, this.debt.debtAmount)
+        },
+        {
+          controlName: 'percentage',
+          type: 'number',
+          // validators: [
+          //   minStrict(promiseMode === 2 && !canAddInsufficientAmount ? this.limit.minAmountPercent : 0),
+          //   max(100),
+          // ],
+          // disabled: promiseMode === 3,
+          // onChange: event => this.onPercentageChange(event, this.debt.debtAmount)
+        },
+      ]
+    },
+    // Payment
+    {
+      title: 'modules.contactRegistration.payment.title',
+      children: [
+        { controlName: 'date', type: 'datepicker', required: true, /* maxDate, */ },
+        {
+          controlName: 'amount',
+          type: 'number',
+          required: true,
+          // disabled: paymentMode === 3,
+          // validators: [ minStrict(0), max(debtAmount) ],
+          // onChange: event => this.onAmountChange(event, debtAmount)
+        },
+        {
+          controlName: 'percentage',
+          type: 'number',
+          // disabled: paymentMode === 3,
+          // validators: [ minStrict(0), max(100) ],
+          // onChange: event => this.onPercentageChange(event, debtAmount)
+        },
+        { controlName: 'currencyId', type: 'selectwrapper', lookupKey: 'currencies', required: true },
+      ]
+    },
+    // Phone
+    {
+      title: 'modules.contactRegistration.phone.title',
+      children: [
+        {
+          controlName: 'typeCode',
+          type: 'selectwrapper',
+          dictCode: UserDictionariesService.DICTIONARY_PHONE_TYPE,
+          required: true,
+        },
+        { controlName: 'phone', type: 'text', required: true },
+        { controlName: 'stopAutoSms', type: 'checkbox' },
+        { controlName: 'stopAutoInfo', type: 'checkbox' },
+        { controlName: 'comment', type: 'textarea' },
+        // Contact registration select
+      ]
+    },
+    // Misc
+    {
+      title: 'modules.contactRegistration.misc.title',
+      children: [
+        { controlName: 'nextCallDateTime', type: 'datepicker', displayTime: true },
+        { controlName: 'callReasonCode', type: 'selectwrapper', dictCode: 49 },
+        { controlName: 'debtReasonCode', type: 'selectwrapper', dictCode: 11 },
+        { controlName: 'refusalReasonCode', type: 'selectwrapper', dictCode: 19, parentCode: 3 },
+        { controlName: 'statusReasonCode', type: 'selectwrapper', dictCode: 19 },
+        { controlName: 'comment', type: 'textarea' },
+        // AutoCommentId
+        // AutoComment
+      ].map(item => ({ ...item, label: labelKey(item.controlName) })) as IDynamicFormControl[]
+    }
+  ];
 
   data = {};
 
