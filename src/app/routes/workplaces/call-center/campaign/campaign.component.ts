220c0485c8ea203c648e9e7fc312b2c6b08ad437
Dmitri Maltsev <dmitri.maltsev@hotmail.com>
2018-01-23 11:45:56 +0300

    refactoring: injecting contact registration service in each container seperately

--- src/app/routes/workplaces/call-center/campaign/campaign.component.ts
+++ src/app/routes/workplaces/call-center/campaign/campaign.component.ts
@@ -3,12 +3,14 @@ import { Observable } from 'rxjs/Observable';
 import { map } from 'rxjs/operators';
 
 import { CampaignService } from './campaign.service';
+import { ContactRegistrationService } from '@app/routes/workplaces/shared/contact-registration/contact-registration.service';
 
 @Component({
   changeDetection: ChangeDetectionStrategy.OnPush,
   host: { class: 'full-height' },
   providers: [
     CampaignService,
+    ContactRegistrationService,
   ],
   selector: 'app-campaign',
   templateUrl: 'campaign.component.html',
