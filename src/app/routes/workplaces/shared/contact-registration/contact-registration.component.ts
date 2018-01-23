d829fbea71e048cfef2143b7769658b43890ff25
Dmitri Maltsev <dmitri.maltsev@hotmail.com>
2018-01-23 12:19:39 +0300

    chore: removed unused imports

--- src/app/routes/workplaces/shared/contact-registration/contact-registration.component.ts
+++ src/app/routes/workplaces/shared/contact-registration/contact-registration.component.ts
@@ -1,4 +1,4 @@
-import { ChangeDetectionStrategy, Component, Input } from '@angular/core';
+import { ChangeDetectionStrategy, Component } from '@angular/core';
 
 import { ContactRegistrationService } from './contact-registration.service';
 import { IContactRegistrationMode } from '@app/routes/workplaces/shared/contact-registration/contact-registration.interface';
@@ -9,36 +9,6 @@ import { IContactRegistrationMode } from '@app/routes/workplaces/shared/contact-
   templateUrl: './contact-registration.component.html',
 })
 export class ContactRegistrationComponent {
-  // @Input('campaignId')
-  // set campaignId(campaignId: number) {
-  //   this.contactRegistrationService.campaignId = campaignId;
-  // }
-
-  // @Input('contactId')
-  // set contactId(contactId: number) {
-  //   this.contactRegistrationService.contactId = contactId;
-  // }
-
-  // @Input('contactType')
-  // set contactType(contactType: number) {
-  //   this.contactRegistrationService.contactType = contactType;
-  // }
-
-  // @Input('debtId')
-  // set debtId(debtId: number) {
-  //   this.contactRegistrationService.debtId = debtId;
-  // }
-
-  // @Input('personId')
-  // set personId(personId: number) {
-  //   this.contactRegistrationService.personId = personId;
-  // }
-
-  // @Input('personRole')
-  // set personRole(personRole: number) {
-  //   this.contactRegistrationService.personRole = personRole;
-  // }
-
   constructor(
     private contactRegistrationService: ContactRegistrationService,
   ) {}
