bfcdafc43e6a742f21dac0ac01fa2d83ef8222ca
Dmitri Maltsev <dmitri.maltsev@hotmail.com>
2018-01-31 11:45:59 +0300

    fix: moment pipe locales

--- src/app/shared/pipes/moment/moment.pipe.ts
+++ src/app/shared/pipes/moment/moment.pipe.ts
@@ -14,26 +14,18 @@ export class MomentPipe implements OnDestroy, PipeTransform {
   constructor(
     private cdRef: ChangeDetectorRef,
     private translateService: TranslateService,
-  ) {}
+  ) {
+    this.langSub = this.translateService.onLangChange.subscribe(() => this.cdRef.markForCheck());
+  }
 
   ngOnDestroy(): void {
     this.langSub.unsubscribe();
   }
 
   transform(value: moment.Moment, format: string): string {
-    const { currentLang, defaultLang, onLangChange } = this.translateService;
+    const { currentLang, defaultLang } = this.translateService;
     const lang = currentLang || defaultLang;
-    this.update(value, format, lang);
-    this.langSub = onLangChange.subscribe(event => this.update(value, format, event.lang));
+    this.formattedValue = value.locale(lang).format(format);
     return this.formattedValue;
-  }
-
-  private update(value: moment.Moment, format: string, lang: string): void {
-    const locales = moment.locales();
-    const locale = locales.includes(lang)
-      ? lang
-      : locales[0];
-    this.formattedValue = value.locale(locale).format(format);
-    this.cdRef.markForCheck();
   }
 }
