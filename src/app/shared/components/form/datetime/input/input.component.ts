928c81fba9e80ce0d706f2f5ed4a9860602f7973
Dmitri Maltsev <dmitri.maltsev@hotmail.com>
2018-02-06 14:12:42 +0300

    fix: picker locales

--- src/app/shared/components/form/datetime/input/input.component.ts
+++ src/app/shared/components/form/datetime/input/input.component.ts
@@ -1,6 +1,9 @@
-import { ChangeDetectionStrategy, ChangeDetectorRef, Component, forwardRef, Input } from '@angular/core';
+import { ChangeDetectionStrategy, ChangeDetectorRef, Component, forwardRef, Input, OnDestroy, OnInit } from '@angular/core';
 import { ControlValueAccessor, NG_VALUE_ACCESSOR } from '@angular/forms';
+import { TranslateService } from '@ngx-translate/core';
+import { Subscription } from 'rxjs/Subscription';
 import * as moment from 'moment';
+import { LongDateFormatKey } from 'moment';
 
 import { DateTimeService } from '../datetime.service';
 
@@ -16,28 +19,47 @@ import { DateTimeService } from '../datetime.service';
   selector: 'app-datetime-input',
   templateUrl: './input.component.html'
 })
-export class DateTimeInputComponent implements ControlValueAccessor {
-  // TODO(d.maltsev): get format from locale
+export class DateTimeInputComponent implements ControlValueAccessor, OnInit, OnDestroy {
   @Input() format: string;
 
   private _disabled = false;
   private _value: Date;
 
+  private langSub: Subscription;
+
   constructor(
     private cdRef: ChangeDetectorRef,
     private dateTimeService: DateTimeService,
+    private translateService: TranslateService,
   ) {}
+
+  ngOnInit(): void {
+    this.langSub = this.translateService.onLangChange.subscribe(() => this.cdRef.markForCheck());
+  }
+
+  ngOnDestroy(): void {
+    this.langSub.unsubscribe();
+  }
 
   get disabled(): boolean {
     return this._disabled;
   }
 
   get mask(): any {
-    return this.dateTimeService.getMaskParamsFromMomentFormat(this.format);
+    return this.dateTimeService.getMaskParamsFromMomentFormat(this.formatString);
   }
 
   get momentValue(): moment.Moment {
     return moment(this._value);
+  }
+
+  get formatString(): string {
+    const { currentLang, defaultLang } = this.translateService;
+    const lang = currentLang || defaultLang;
+    return this.format
+      .split(' ')
+      .map(format => moment.localeData(lang).longDateFormat(format as LongDateFormatKey) || format)
+      .join(' ');
   }
 
   writeValue(value: Date): void {
@@ -70,7 +92,7 @@ export class DateTimeInputComponent implements ControlValueAccessor {
 
   onChange(event: Event): void {
     const { value } = event.target as HTMLInputElement;
-    const date = moment(value, this.format);
+    const date = moment(value, this.formatString);
     if (date.isValid()) {
       this.update(date.toDate());
     }
