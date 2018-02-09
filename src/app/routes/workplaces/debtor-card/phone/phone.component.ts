db703bcaf85150c69d7f39c275de902a1d3bad01
Igor Kibisov <donar061@gmail.com>
2018-02-09 15:19:38 +0300

    ref: phone card back
    #refs WEB20-596

--- src/app/routes/workplaces/debtor-card/phone/phone.component.ts
+++ src/app/routes/workplaces/debtor-card/phone/phone.component.ts
@@ -4,6 +4,7 @@ import { combineLatest } from 'rxjs/observable/combineLatest';
 import { Observable } from 'rxjs/Observable';
 
 import { DebtorCardService } from '../../../../core/app-modules/debtor-card/debtor-card.service';
+import { RoutingService } from '@app/core/routing/routing.service';
 
 interface PhoneCardRouteParams {
   contactId: number;
@@ -18,6 +19,7 @@ export class DebtorPhoneComponent {
   constructor(
     private debtorCardService: DebtorCardService,
     private route: ActivatedRoute,
+    private routingService: RoutingService
   ) {}
 
   get phoneId$(): Observable<number> {
@@ -31,5 +33,15 @@ export class DebtorPhoneComponent {
 
   get routeParams$(): Observable<PhoneCardRouteParams> {
     return this.route.params as Observable<PhoneCardRouteParams>;
+  }
+
+  onClose(): void {
+    const contactId = this.route.snapshot.paramMap.get('contactId');
+    this.routingService.navigate([
+      '/workplaces',
+      'debtor-card',
+      this.route.snapshot.paramMap.get('debtId'),
+      ...(contactId ? [ 'contact', contactId ] : [])
+    ]);
   }
 }
