b20d0c58666cd003d3d8c87ad1f77e44e854c412
Dmitri Maltsev <dmitri.maltsev@hotmail.com>
2018-02-12 19:10:12 +0300

    chore: e2e jest setup

---
+++ e2e/tests/foo.e2e-spec.ts
@@ -0,0 +1,20 @@
+import * as Puppeteer from 'puppeteer';
+import { toMatchImageSnapshot } from 'jest-image-snapshot';
+
+jest.setTimeout(32000);
+
+expect.extend({ toMatchImageSnapshot });
+
+describe('foo', () => {
+  it('should find the nightmare github link first', async (done) => {
+    const page = await global['__BROWSER__'].newPage();
+    await page.goto('https://duckduckgo.com');
+    await page.type('#search_form_input_homepage', 'github nightmare');
+    await page.click('#search_button_homepage');
+    await page.waitForNavigation();
+    const screenshot = await page.screenshot();
+    await page.close();
+    expect(screenshot)['toMatchImageSnapshot']();
+    done();
+  });
+});
