f75064ba27f7c92c8324485b7b2fb0166ed5b073
Igor Kibisov <donar061@gmail.com>
2018-02-13 13:15:05 +0300

    feat: group events grid
    #refs WEB20-789

---
+++ src/app/routes/utilities/schedule/groups/groups.component.ts
@@ -0,0 +1,9 @@
+import { ChangeDetectionStrategy, Component } from '@angular/core';
+
+@Component({
+  selector: 'app-groups',
+  templateUrl: './groups.component.html',
+  changeDetection: ChangeDetectionStrategy.OnPush,
+})
+export class GroupsComponent {
+}
