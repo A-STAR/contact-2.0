252101f329518e65ea0166b192d1761143805b08
Dmitry Topheenko <topheenko.dmitry@gmail.com>
2018-02-14 13:22:52 +0300

    fix: phone and address creation

--- src/app/routes/workplaces/call-center/campaign/phone/phone.component.ts
+++ src/app/routes/workplaces/call-center/campaign/phone/phone.component.ts
@@ -1,7 +1,14 @@
 import { ChangeDetectionStrategy, Component } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
+import { Observable } from 'rxjs/Observable';
+import { map } from 'rxjs/operators';
 
 import { RoutingService } from '@app/core/routing/routing.service';
+
+interface PhoneCardRouteParams {
+  personId: number;
+  phoneId: number;
+}
 
 @Component({
   selector: 'app-call-center-phone',
@@ -14,6 +21,18 @@ export class PhoneComponent {
     private route: ActivatedRoute,
     private routingService: RoutingService
   ) {}
+
+  get phoneId$(): Observable<number> {
+    return this.routeParams$.pipe(map(params => params.phoneId));
+  }
+
+  get entityId$(): Observable<number> {
+    return this.routeParams$.pipe(map(params => params.personId));
+  }
+
+  get routeParams$(): Observable<PhoneCardRouteParams> {
+    return this.route.params as Observable<PhoneCardRouteParams>;
+  }
 
   onClose(): void {
     this.routingService.navigate([
