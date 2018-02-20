173f24d951a4c4a38ea9085f6c1786bd5cd8e22b
Ivan Lobanov <arkhemlol@gmail.com>
2018-02-20 19:08:52 +0300

    ref: move identity
    #refs WEB20-869

---
+++ src/app/routes/workplaces/debtor-card/identity/identity/identity-grid/identity-grid.component.ts
@@ -0,0 +1,199 @@
+import {
+  ChangeDetectionStrategy, ChangeDetectorRef, Component,
+  ViewChild, OnDestroy, OnInit, Input, Output, EventEmitter
+} from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { BehaviorSubject } from 'rxjs/BehaviorSubject';
+import { first } from 'rxjs/operators';
+import { combineLatest } from 'rxjs/observable/combineLatest';
+import { Observable } from 'rxjs/Observable';
+import { Subscription } from 'rxjs/Subscription';
+
+import { IGridColumn } from '@app/shared/components/grid/grid.interface';
+import { IToolbarItem, ToolbarItemTypeEnum } from '@app/shared/components/toolbar-2/toolbar-2.interface';
+import { IIdentityDoc } from '@app/shared/gui-objects/widgets/identity/identity.interface';
+
+import { GridService } from '@app/shared/components/grid/grid.service';
+import { IdentityService } from '@app/shared/gui-objects/widgets/identity/identity.service';
+import { NotificationsService } from '@app/core/notifications/notifications.service';
+import { UserDictionariesService } from '@app/core/user/dictionaries/user-dictionaries.service';
+import { UserPermissionsService } from '@app/core/user/permissions/user-permissions.service';
+
+import { GridComponent } from '@app/shared/components/grid/grid.component';
+
+import { combineLatestAnd } from '@app/core/utils/helpers';
+import { DialogFunctions } from '@app/core/dialog';
+
+@Component({
+  changeDetection: ChangeDetectionStrategy.OnPush,
+  selector: 'app-identity-grid',
+  templateUrl: './identity-grid.component.html',
+})
+export class IdentityGridComponent extends DialogFunctions implements OnInit, OnDestroy {
+  @ViewChild(GridComponent) grid: GridComponent;
+
+  private routeParams = this.route.snapshot.paramMap;
+
+  @Input()
+  set personId(personId: number) {
+    this._personId$.next(personId);
+    this.cdRef.markForCheck();
+  }
+
+  @Output() add = new EventEmitter<void>();
+  @Output() dblClick = new EventEmitter<IIdentityDoc>();
+  @Output() edit = new EventEmitter<IIdentityDoc>();
+
+  private _personId$ = new BehaviorSubject<number>(null);
+
+  private selectedRows$ = new BehaviorSubject<IIdentityDoc[]>([]);
+
+  dialog: string;
+  gridStyles = this.routeParams.get('contactId') ? { height: '230px' } : { height: '500px' };
+  toolbarClass = !this.routeParams.get('contactId') ? 'bh' : 'bordered';
+  onSaveSubscription: Subscription;
+  canViewSubscription: Subscription;
+
+  identityDoc: IIdentityDoc;
+  rows: IIdentityDoc[] = [];
+
+  columns: Array<IGridColumn> = [
+    { prop: 'docTypeCode', minWidth: 70, type: 'number', dictCode: UserDictionariesService.DICTIONARY_IDENTITY_TYPE },
+    { prop: 'docNumber', type: 'string', minWidth: 70 },
+    { prop: 'issueDate', type: 'date', renderer: 'dateRenderer', width: 110 },
+    { prop: 'issuePlace', type: 'string' },
+    { prop: 'expiryDate', type: 'date', renderer: 'dateRenderer', width: 110 },
+    { prop: 'citizenship', type: 'string' },
+    { prop: 'isMain', width: 70 , renderer: 'checkboxRenderer' },
+  ];
+
+  toolbarItems: Array<IToolbarItem> = [
+    {
+      type: ToolbarItemTypeEnum.BUTTON_ADD,
+      enabled: combineLatestAnd([this.canAdd$, this._personId$.map(Boolean)]),
+      action: () => this.onAdd()
+    },
+    {
+      type: ToolbarItemTypeEnum.BUTTON_EDIT,
+      enabled: combineLatestAnd([this.canEdit$, this.selectedRows$.map(s => !!s.length)]),
+      action: () => this.onEdit(this.identityDoc)
+    },
+    {
+      type: ToolbarItemTypeEnum.BUTTON_DELETE,
+      enabled: combineLatestAnd([this.canDelete$, this.selectedRows$.map(s => !!s.length)]),
+      action: () => this.setDialog('removeIdentity')
+    },
+    {
+      type: ToolbarItemTypeEnum.BUTTON_REFRESH,
+      enabled: combineLatestAnd([this.canView$, this._personId$.map(Boolean)]),
+      action: () => this.fetch()
+    },
+  ];
+
+  constructor(
+    private cdRef: ChangeDetectorRef,
+    private gridService: GridService,
+    private identityService: IdentityService,
+    private notificationsService: NotificationsService,
+    private route: ActivatedRoute,
+    private userPermissionsService: UserPermissionsService
+  ) {
+    super();
+    this.onSubmitSuccess = this.onSubmitSuccess.bind(this);
+  }
+
+  ngOnInit(): void {
+    this.gridService.setAllRenderers(this.columns)
+      .pipe(first())
+      .subscribe(columns => {
+        this.columns = [...columns];
+        this.cdRef.markForCheck();
+      });
+
+    this.onSaveSubscription = this.identityService
+      .getAction(IdentityService.DEBTOR_IDENTITY_SAVED)
+      .subscribe(() => this.fetch());
+
+    this.canViewSubscription = combineLatest(
+      this.canView$,
+      this._personId$.filter(Boolean)
+    )
+    .subscribe(([ canView ]) => {
+      if (canView) {
+        this.fetch();
+      } else {
+          this.notificationsService.permissionError().entity('entities.identityDocs.gen.plural').dispatch();
+        this.clear();
+      }
+    });
+  }
+
+  ngOnDestroy(): void {
+    this.selectedRows$.complete();
+    this.canViewSubscription.unsubscribe();
+    this.onSaveSubscription.unsubscribe();
+  }
+
+  fetch(): void {
+    if (this._personId$.value) {
+      this.identityService
+        .fetchAll(this._personId$.value)
+        .subscribe(identities => {
+          this.rows = [...identities];
+          this.selectedRows$.next([]);
+          this.cdRef.markForCheck();
+        });
+    }
+  }
+
+  onRemove(): void {
+    this.identityService.delete(this._personId$.value, this.grid.selected[0].id)
+      .subscribe(this.onSubmitSuccess);
+  }
+
+  onSelect(doc: IIdentityDoc): void {
+    this.identityDoc = doc;
+    this.selectedRows$.next(this.grid.selected);
+  }
+
+  onDoubleClick(doc: IIdentityDoc): void {
+    this.dblClick.emit(doc);
+  }
+
+  get canView$(): Observable<boolean> {
+    return this.userPermissionsService.has('IDENTITY_DOCUMENT_VIEW');
+  }
+
+  get canAdd$(): Observable<boolean> {
+    return this.userPermissionsService.has('IDENTITY_DOCUMENT_ADD');
+  }
+
+  get canEdit$(): Observable<boolean> {
+    return this.userPermissionsService.has('IDENTITY_DOCUMENT_EDIT');
+  }
+
+  get canDelete$(): Observable<boolean> {
+    return this.userPermissionsService.has('IDENTITY_DOCUMENT_DELETE');
+  }
+
+  private onAdd(): void {
+    this.add.emit();
+  }
+
+  private onEdit(doc: IIdentityDoc): void {
+    this.edit.emit(doc);
+  }
+
+  private clear(): void {
+    this.rows = [];
+    this.cdRef.markForCheck();
+  }
+
+  private onSubmitSuccess(result: boolean): void {
+    if (result) {
+      this.fetch();
+      this.setDialog();
+    }
+  }
+
+}
---
+++ src/app/routes/workplaces/debtor-card/identity/identity/identity.service.ts
@@ -0,0 +1,51 @@
+import { Actions } from '@ngrx/effects';
+import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs/Observable';
+import { Store } from '@ngrx/store';
+
+import { IAppState } from '../../../../core/state/state.interface';
+import { IIdentityDoc } from './identity.interface';
+
+import { AbstractActionService } from '../../../../core/state/action.service';
+import { DataService } from '../../../../core/data/data.service';
+
+@Injectable()
+export class IdentityService extends AbstractActionService {
+  static DEBTOR_IDENTITY_SAVED = 'DEBTOR_IDENTITY_SAVED';
+
+  hasMain = false;
+  private url = '/persons/{personId}/identitydocuments';
+
+  constructor(
+    protected actions: Actions,
+    private dataService: DataService,
+    protected store: Store<IAppState>,
+  ) {
+    super();
+  }
+
+  fetchAll(personId: number): Observable<any> {
+    return this.dataService.readAll('/persons/{personId}/identitydocuments', { personId })
+      .do(identityDocs => this.checkForMain(identityDocs));
+  }
+
+  fetch(personId: number, docId: number): Observable<IIdentityDoc> {
+    return this.dataService.read(`${this.url}/{docId}`, { personId, docId });
+  }
+
+  create(personId: number, doc: IIdentityDoc): Observable<boolean> {
+    return this.dataService.create(this.url, { personId }, doc);
+  }
+
+  delete(personId: number, docId: number): Observable<boolean> {
+    return this.dataService.delete(`${this.url}/{docId}`, { personId, docId });
+  }
+
+  update(personId: number, docId: number, doc: IIdentityDoc): Observable<boolean> {
+    return this.dataService.update(`${this.url}/{docId}`, { personId, docId }, doc);
+  }
+
+  private checkForMain(identityDocs: IIdentityDoc[]): void {
+    this.hasMain = identityDocs.some(doc => Boolean(doc.isMain));
+  }
+}
