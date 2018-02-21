9ecda8b955e321eb82503f804729bff895c37f3f
Dmitri Maltsev <dmitri.maltsev@hotmail.com>
2018-02-21 12:47:54 +0300

    chore: replaced promises grid

--- src/app/routes/workplaces/shared/promise/grid/promise-grid.module.ts
+++ src/app/routes/workplaces/shared/promise/grid/promise-grid.module.ts
@@ -1,27 +1,18 @@
 import { NgModule } from '@angular/core';
-import { CommonModule } from '@angular/common';
 
-import { DialogActionModule } from '@app/shared/components/dialog-action/dialog-action.module';
-import { GridModule } from '@app/shared/components/grid/grid.module';
-import { Toolbar2Module } from '@app/shared/components/toolbar-2/toolbar-2.module';
+import { SharedModule } from '@app/shared/shared.module';
 
 import { PromiseGridComponent } from './promise-grid.component';
 
 @NgModule({
   imports: [
-    CommonModule,
-    DialogActionModule,
-    GridModule,
-    Toolbar2Module,
+    SharedModule,
   ],
   exports: [
     PromiseGridComponent,
   ],
   declarations: [
     PromiseGridComponent,
-  ],
-  entryComponents: [
-    PromiseGridComponent,
   ]
 })
-export class PromiseGridModule { }
+export class PromiseGridModule {}
