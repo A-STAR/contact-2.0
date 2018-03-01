407bf8db210980bbda342f55c7f0856b8188b3f0
Ivan Lobanov <arkhemlol@gmail.com>
2018-03-01 18:05:09 +0300

    feat: register contact mass op boilerplate
    #refs WEB20-747

---
+++ src/app/shared/mass-ops/register-contact/register-contact.module.ts
@@ -0,0 +1,11 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { OpenRegisterContactComponent } from './open-register-contact.component';
+
+@NgModule({
+  imports: [
+    CommonModule
+  ],
+  declarations: [OpenRegisterContactComponent]
+})
+export class RegisterContactModule { }
