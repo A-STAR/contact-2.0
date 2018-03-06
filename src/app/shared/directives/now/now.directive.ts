a8306e8811240aba0e29d0bcbafc137263e9fd96
Ivan Lobanov <arkhemlol@gmail.com>
2018-03-06 14:23:16 +0300

    fix: added locale to appNow directive
    #refs WEB20-929

--- src/app/shared/directives/now/now.directive.ts
+++ src/app/shared/directives/now/now.directive.ts
@@ -1,4 +1,7 @@
 import { OnInit, OnDestroy, Directive, Input, ElementRef, NgZone } from '@angular/core';
+import { Subscription } from 'rxjs/Subscription';
+import { TranslateService, LangChangeEvent } from '@ngx-translate/core';
+
 import * as moment from 'moment';
 
 @Directive({
@@ -8,13 +11,22 @@ export class NowDirective implements OnInit, OnDestroy {
   @Input() format;
 
   intervalId;
+  private currentLang: string;
+  private langSub: Subscription;
 
   constructor(
     private element: ElementRef,
+    private translateService: TranslateService,
     private zone: NgZone,
   ) {}
 
   ngOnInit(): void {
+
+    this.currentLang = this.translateService.currentLang || this.translateService.defaultLang;
+    this.langSub = this.translateService.onLangChange.subscribe((params: LangChangeEvent) => {
+      this.currentLang = params.lang;
+    });
+
     this.updateTime();
     this.zone.runOutsideAngular(() => {
       this.intervalId = setInterval(() => {
@@ -24,11 +36,14 @@ export class NowDirective implements OnInit, OnDestroy {
   }
 
   updateTime(): void {
-    const dt = moment().format(this.format);
+    const dt = moment().locale(this.currentLang).format(this.format);
     this.element.nativeElement.innerHTML = dt;
   }
 
   ngOnDestroy(): void {
     clearInterval(this.intervalId);
+    if (this.langSub) {
+      this.langSub.unsubscribe();
+    }
   }
 }
