1500c0646a6eb373d6deb6e0a32b5b0ae54048d3
Ivan Lobanov <arkhemlol@gmail.com>
2018-03-06 16:23:56 +0300

    ref: added test for callback renderer cmp
    #refs WEB20-917

---
+++ src/app/shared/components/grids/renderers/callback/callback.component.spec.ts
@@ -0,0 +1,50 @@
+import { async as Async, ComponentFixture, TestBed } from '@angular/core/testing';
+import { Observable } from 'rxjs/Observable';
+import { of } from 'rxjs/observable/of';
+
+import { CallbackRendererComponent } from './callback.component';
+
+describe('CallbackCellRendererComponent', () => {
+  let fixture: ComponentFixture<CallbackRendererComponent>;
+  const mockCallback = (value: any) => (value || { renderCase: 'defaultString' }).renderCase;
+  beforeEach(Async(() => {
+    TestBed
+      .configureTestingModule({
+        declarations: [
+          CallbackRendererComponent,
+        ],
+      })
+      .compileComponents();
+  }));
+
+  beforeEach(() => {
+    fixture = TestBed.createComponent(CallbackRendererComponent);
+  });
+
+  it('should invoke passed renderer callback', () => {
+    const agGridParams = {
+      rendererCallback: mockCallback,
+      value: null,
+    };
+
+    const spy = spyOn(agGridParams, 'rendererCallback')
+    .and
+    .returnValue('');
+
+    fixture.componentInstance.agInit(agGridParams as any);
+    fixture.detectChanges();
+    expect(fixture.nativeElement).toMatchSnapshot();
+    expect(spy).toHaveBeenCalled();
+  });
+
+  it('should render transformed value passed through callback', () => {
+    fixture.componentInstance.agInit({
+      rendererCallback: mockCallback,
+      data: {
+        renderCase: 'myFancyString',
+      },
+    } as any);
+    fixture.detectChanges();
+    expect(fixture.nativeElement).toMatchSnapshot();
+  });
+});
