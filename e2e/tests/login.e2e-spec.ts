36c3124088f661676039354fcf2d4635e8cf7a08
Dmitri Maltsev <dmitri.maltsev@hotmail.com>
2018-03-13 17:59:11 +0300

    chore: larger viewport for e2e

--- e2e/tests/login.e2e-spec.ts
+++ e2e/tests/login.e2e-spec.ts
@@ -7,6 +7,7 @@ expect.extend({ toMatchImageSnapshot });
 describe('Login Page', () => {
   it('should render', async (done) => {
     const page = await global['__BROWSER__'].newPage();
+    await page.setViewport({ width: 1600, height: 1200 });
     await page.goto('http://appservertest.luxbase.int:4100');
     const screenshot = await page.screenshot();
     await page.close();
