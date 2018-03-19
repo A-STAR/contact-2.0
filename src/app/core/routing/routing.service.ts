92ae5a0d3da0fd995d74e0de83b39d74e4658000
Ivan Lobanov <arkhemlol@gmail.com>
2018-03-19 14:37:28 +0300

    ref: navigate returns promise
    #refs WEB20-938

--- src/app/core/routing/routing.service.ts
+++ src/app/core/routing/routing.service.ts
@@ -8,17 +8,21 @@ export class RoutingService {
     private router: Router,
   ) {}
 
-  navigate(segments: string[], route: ActivatedRoute = this.route): void {
-    this.navigateRecursively(segments, 1, route);
+  navigate(segments: string[], route: ActivatedRoute = this.route): Promise<boolean> {
+    const defer = new Promise<boolean>(resolve => this.navigateRecursively(segments, 1, route, resolve));
+    return defer;
   }
 
-  private navigateRecursively(segments: string[], i: number, route: ActivatedRoute): void {
+  private navigateRecursively(segments: string[], i: number, route: ActivatedRoute, resolve: (state: boolean) => void): void {
     const segmentsSlice = segments.slice(0, i);
     const isLastSegment = segmentsSlice.length === segments.length;
     const url = segmentsSlice.join('/');
-    this.router.navigate([ url ], { replaceUrl: isLastSegment, relativeTo: route }).then(() => {
+
+    this.router.navigate([ url ], { replaceUrl: isLastSegment, relativeTo: route }).then(isSucceeded => {
       if (!isLastSegment) {
-        this.navigateRecursively(segments, i + 1, route);
+        this.navigateRecursively(segments, i + 1, route, resolve);
+      } else {
+        resolve(isSucceeded);
       }
     });
   }
