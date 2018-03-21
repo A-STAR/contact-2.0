ce13c63e23efbbac80c270d5ca9d78970fdf43cc
Ivan Lobanov <arkhemlol@gmail.com>
2018-03-21 20:25:22 +0300

    chore: fixed test
    #refs WEB20-1255

--- src/app/shared/components/action-grid/action-grid.service.spec.ts
+++ src/app/shared/components/action-grid/action-grid.service.spec.ts
@@ -1,15 +1,42 @@
 import { TestBed, inject } from '@angular/core/testing';
 
 import { ActionGridService } from './action-grid.service';
+import { MassOperationsService } from '@app/shared/mass-ops/mass-ops.service';
+
+class MockMassOpsService {
+  nonDlgActions = {
+    openDebtCard: () => null,
+    openDebtCardByDebtor: () => null,
+    openIncomingCall: () => null
+  };
+
+  openDebtCard(): void {}
+  openDebtCardByDebtor(): void {}
+  openIncomingCall(): void {}
+
+}
 
 describe('ActionGridService', () => {
+  // let service: ActionGridService;
+  // let massOpsService: MassOperationsService;
+
   beforeEach(() => {
     TestBed.configureTestingModule({
-      providers: [ActionGridService]
+      providers: [
+        ActionGridService,
+        {
+          provide: MassOperationsService,
+          useClass: MockMassOpsService,
+        },
+      ]
     });
+
+    // service = TestBed.get(ActionGridService);
+    // massOpsService = TestBed.get(MassOperationsService);
   });
 
-  it('should be created', inject([ActionGridService], (service: ActionGridService) => {
-    expect(service).toBeTruthy();
+  it('should be created', inject([ActionGridService], (s: ActionGridService) => {
+    expect(s).toBeTruthy();
   }));
+
 });
--- src/app/shared/components/action-grid/filter/action-grid-filter.service.ts
+++ src/app/shared/components/action-grid/filter/action-grid-filter.service.ts
@@ -3,10 +3,6 @@ import { BehaviorSubject } from 'rxjs/BehaviorSubject';
 
 @Injectable()
 export class ActionGridFilterService {
-
   // notify subscribers, that grid has filters
   hasFilter$ = new BehaviorSubject<boolean>(null);
-
-  constructor() {}
-
 }
