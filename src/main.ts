4c5c2ddb88e2cf78948669a9d9fa701165b65965
Dmitri Maltsev <dmitri.maltsev@hotmail.com>
2018-03-22 11:10:21 +0300

    refactoring: app entry point

--- src/main.ts
+++ src/main.ts
@@ -17,17 +17,23 @@ if (environment.production) {
 // tslint:disable-next-line
 LicenseManager.setLicenseKey('CRIF_LLC_Contact_Collection_Solution_1Devs19_February_2019__MTU1MDUzNDQwMDAwMA==19f9da3e1e632091854eee1e76adf484');
 
+const configUrl = '/assets/server/root.json';
+
 // This isn't pretty but there is no other way to inject values dynamically
 const request = new XMLHttpRequest();
 request.onload = () => {
   const { response } = request;
   if (!response) {
-    throw new Error('Could not get config');
+    throw new Error(`
+      Could not get config at ${configUrl}.
+    `);
   }
 
   const domain = response.url.match(/:\/\/(.+)$/)[1];
   if (!domain) {
-    throw new Error('Could not get domain config');
+    throw new Error(`
+      Could not extract whitelisted domains from config at ${configUrl}.
+    `);
   }
 
   const config: IConfig = {
@@ -43,6 +49,6 @@ request.onload = () => {
   platformBrowserDynamic().bootstrapModule(AppModule).then(() => {});
 };
 
-request.open('GET', '/assets/server/root.json');
+request.open('GET', configUrl);
 request.responseType = 'json';
 request.send();
