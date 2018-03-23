77d3e91f5e5cc94f7e7da2b17bef75e54766702d
Ivan Lobanov <arkhemlol@gmail.com>
2018-03-23 23:11:55 +0300

    feat: change filter in group debts grid
    #refs WEB20-1264

--- src/app/routes/utilities/schedule/group-debts/group-debts.component.ts
+++ src/app/routes/utilities/schedule/group-debts/group-debts.component.ts
@@ -4,21 +4,15 @@ import {
   Component,
   OnInit,
   ViewChild,
-  Inject,
 } from '@angular/core';
 
 import { IAGridResponse } from '@app/shared/components/grid2/grid2.interface';
-import { IDynamicFormConfig, IDynamicFormControl } from '@app/shared/components/form/dynamic-form/dynamic-form.interface';
-import { IFilterGroup } from '@app/core/filters/grid-filters.interface';
+import { IDynamicFormControl } from '@app/shared/components/form/dynamic-form/dynamic-form.interface';
 import { IGroupDebt } from './group-debts.interface';
 
-import { GridFiltersService } from '@app/core/filters/grid-filters.service';
 import { GroupDebtsService } from './group-debts.service';
 
 import { ActionGridComponent } from '@app/shared/components/action-grid/action-grid.component';
-import { DynamicFormComponent } from '@app/shared/components/form/dynamic-form/dynamic-form.component';
-
-import { addGridLabel } from '@app/core/utils';
 
 @Component({
   selector: 'app-group-debts',
@@ -29,80 +23,35 @@ import { addGridLabel } from '@app/core/utils';
 })
 export class GroupDebtsComponent implements OnInit {
   @ViewChild(ActionGridComponent) grid: ActionGridComponent<IGroupDebt>;
-  @ViewChild(DynamicFormComponent) form: DynamicFormComponent;
 
-  groupId: number;
   rowIdKey = 'debtId';
   rowCount = 0;
   rows: any[] = [];
-
-  config: IDynamicFormConfig = {
-    labelKey: 'widgets.groups.groupObjectDebts.filter'
-  };
 
   controls: IDynamicFormControl[];
 
   constructor(
     private cdRef: ChangeDetectorRef,
-    private groupDebtsService: GroupDebtsService,
-    private gridFiltersService: GridFiltersService,
-    @Inject(GroupDebtsService.ENTITY_GROUP_ID) private entityTypeId: number[]
+    private groupDebtsService: GroupDebtsService
   ) { }
 
   ngOnInit(): void {
-    this.gridFiltersService
-      .fetchEntitiesGroups(this.entityTypeId, 0)
-      .subscribe(groups => {
-        this.controls = this.getControls(groups);
-        this.cdRef.markForCheck();
-      });
+
   }
 
   onRequest(): void {
-    if (this.groupId) {
-      const filters = this.grid.getFilters();
+    const filters = this.grid.getFilters();
+    const groupId = filters.get('groupId');
+    if (groupId && groupId.length) {
       const params = this.grid.getRequestParams();
-
       this.groupDebtsService
-        .fetch(this.groupId, filters, params)
+        .fetch(groupId[0], filters, params)
         .subscribe((response: IAGridResponse<IGroupDebt>) => {
           this.rows = [...response.data];
           this.rowCount = response.total;
           this.cdRef.markForCheck();
         });
     }
-  }
-
-  onSearch(): void {
-    const { groups } = this.form.serializedUpdates;
-    this.groupId = groups;
-    this.onRequest();
-  }
-
-  private getControls(groups: IFilterGroup[]): IDynamicFormControl[] {
-    return [
-      {
-        controlName: 'groups',
-        type: 'gridselect',
-        gridColumns: [
-          { prop: 'id', minWidth: 50, maxWidth: 50 },
-          { prop: 'name', minWidth: 300 }
-        ].map(addGridLabel('widgets.groups.groupObjectDebts.filter.grid')),
-        gridRows: groups,
-        gridLabelGetter: (row: IFilterGroup) => row.name,
-        gridValueGetter: (row: IFilterGroup) => row.id,
-        gridOnSelect: (row: IFilterGroup) => this.form.form.patchValue({ groups: row && row.id }),
-        width: 5
-      },
-      // {
-      //   label: 'default.buttons.search',
-      //   controlName: 'searchBtn',
-      //   type: 'searchBtn',
-      //   iconCls: 'fa-search',
-      //   width: 3,
-      //   action: () => this.onSearch()
-      // }
-    ];
   }
 
 }
