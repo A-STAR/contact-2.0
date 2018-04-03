40f8c13dec8c822e40cf0f5a697a22ca99cfb8c7
Dmitri Maltsev <dmitri.maltsev@hotmail.com>
2018-04-03 12:58:33 +0300

    fix: linter rule

--- src/app/core/help/help.service.ts
+++ src/app/core/help/help.service.ts
@@ -3,6 +3,7 @@ import { Injectable } from '@angular/core';
 @Injectable()
 export class HelpService {
   open(guiObjectId: number): void {
+    // tslint:disable-next-line:no-console
     console.log(`STUB: opening help for guiObjectId = ${guiObjectId}`);
   }
 }
