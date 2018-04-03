8c609c895ed08064c3c1944da48b7a684a11e5ba
Dmitri Maltsev <dmitri.maltsev@hotmail.com>
2018-04-03 16:32:17 +0300

    fix: dict loading

--- src/app/core/notifications/notifications.service.ts
+++ src/app/core/notifications/notifications.service.ts
@@ -6,7 +6,7 @@ import { Observable } from 'rxjs/Observable';
 import { Subscription } from 'rxjs/Subscription';
 import { combineLatest } from 'rxjs/observable/combineLatest';
 import { of } from 'rxjs/observable/of';
-import { delay, filter, map, mergeMap, throttleTime } from 'rxjs/operators';
+import { delay, filter, first, map, mergeMap, throttleTime } from 'rxjs/operators';
 import * as moment from 'moment';
 
 import { IAppState } from '../state/state.interface';
@@ -76,6 +76,7 @@ export class NotificationsService implements OnDestroy {
         return this.userDictionariesService
           .getDictionary(UserDictionariesService.DICTIONARY_TASK_TYPE)
           .pipe(
+            first(),
             map(terms => ({ terms, event }))
           );
       }),
