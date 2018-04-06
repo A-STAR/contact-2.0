6657b1df302d24e0ebc0384946c54e7b67eccb3d
Ivan Lobanov <arkhemlol@gmail.com>
2018-04-06 15:42:01 +0300

    fix: contact select grid and form validation
    #refs WEB20-1251

--- src/app/routes/workplaces/shared/contact-registration/edit/contact-select/card/contact-select-card.component.ts
+++ src/app/routes/workplaces/shared/contact-registration/edit/contact-select/card/contact-select-card.component.ts
@@ -25,7 +25,6 @@ export class ContactSelectCardComponent {
     {
       controlName: 'personTypeCode',
       dictCode: UserDictionariesService.DICTIONARY_PERSON_TYPE,
-      markAsDirty: true,
       required: true,
       type: 'select',
     },
