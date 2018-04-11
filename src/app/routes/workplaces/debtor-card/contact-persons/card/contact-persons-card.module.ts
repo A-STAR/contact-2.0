0d3d461009f1411330022246b8c9f44aa7f1ee33
Dmitri Maltsev <dmitri.maltsev@hotmail.com>
2018-04-11 15:58:57 +0300

    refactoring: workplaces shared components

--- src/app/routes/workplaces/debtor-card/contact-persons/card/contact-persons-card.module.ts
+++ src/app/routes/workplaces/debtor-card/contact-persons/card/contact-persons-card.module.ts
@@ -1,8 +1,8 @@
 import { NgModule } from '@angular/core';
 
 import { AddressGridModule } from '@app/routes/workplaces/shared/address/grid/address-grid.module';
-import { IdentityGridModule } from '@app/routes/workplaces/debtor-card/identity/grid/identity-grid.module';
-import { EmploymentGridModule } from '@app/routes/workplaces/debtor-card/employment/grid/employment-grid.module';
+import { EmploymentGridModule } from '@app/routes/workplaces/shared/employment/grid/employment-grid.module';
+import { IdentityGridModule } from '@app/routes/workplaces/shared/identity/grid/identity-grid.module';
 import { PersonSelectModule } from '@app/routes/workplaces/debtor-card/contact-persons/card/person-select/person-select.module';
 import { PhoneGridModule } from '@app/routes/workplaces/shared/phone/grid/phone-grid.module';
 import { SharedModule } from '@app/shared/shared.module';
