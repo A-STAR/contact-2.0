cb26894b2709538c861430dd56e7f0dc4d24ac1f
Dmitri Maltsev <dmitri.maltsev@hotmail.com>
2018-04-11 16:28:44 +0300

    feat: person search filter boilerplate

--- src/app/routes/workplaces/debtor-card/pledge/card-2/dynamic-popups/select-person/filter/select-person-filter.component.ts
+++ src/app/routes/workplaces/debtor-card/pledge/card-2/dynamic-popups/select-person/filter/select-person-filter.component.ts
@@ -1,8 +1,29 @@
 import { ChangeDetectionStrategy, Component } from '@angular/core';
 
+import {
+  IMetadataFormConfig,
+  IMetadataFormControlType,
+} from '@app/shared/components/form/metadata-form/metadata-form.interface';
+
 @Component({
   changeDetection: ChangeDetectionStrategy.OnPush,
   selector: 'app-pledge-card-select-person-filter',
   templateUrl: 'select-person-filter.component.html'
 })
-export class SelectPersonFilterComponent {}
+export class SelectPersonFilterComponent {
+  readonly filterForm: IMetadataFormConfig = {
+    editable: true,
+    items: [
+      {
+        disabled: false,
+        display: true,
+        label: 'ID',
+        name: 'id',
+        type: IMetadataFormControlType.TEXT,
+        validators: {},
+        width: 0,
+      },
+    ],
+    plugins: [],
+  };
+}
