c9014f3a0ac2aa6a907ebb8534f32deb43e2cfc7
Dmitri Maltsev <dmitri.maltsev@hotmail.com>
2018-04-11 16:40:32 +0300

    feat: filter form

--- src/app/routes/workplaces/debtor-card/pledge/card-2/dynamic-popups/select-person/filter/select-person-filter.component.ts
+++ src/app/routes/workplaces/debtor-card/pledge/card-2/dynamic-popups/select-person/filter/select-person-filter.component.ts
@@ -1,9 +1,22 @@
 import { ChangeDetectionStrategy, Component } from '@angular/core';
 
 import {
+  IContextConfigItemType,
+  IContextConfigOperator,
+  IContextByEntityMethod,
+  IContextByValueBagMethod,
+} from '@app/core/context/context.interface';
+
+import {
   IMetadataFormConfig,
   IMetadataFormControlType,
+  IFormContextConfigOperator,
+  IMetadataFormTextControl,
 } from '@app/shared/components/form/metadata-form/metadata-form.interface';
+
+import { UserDictionariesService } from '@app/core/user/dictionaries/user-dictionaries.service';
+
+import { range } from '@app/core/utils';
 
 @Component({
   changeDetection: ChangeDetectionStrategy.OnPush,
@@ -20,6 +33,120 @@ export class SelectPersonFilterComponent {
         label: 'ID',
         name: 'id',
         type: IMetadataFormControlType.TEXT,
+        validators: {},
+        width: 0,
+      },
+      {
+        disabled: false,
+        display: true,
+        label: 'Фамилия/Название',
+        name: 'lastName',
+        type: IMetadataFormControlType.TEXT,
+        validators: {},
+        width: 0,
+      },
+      {
+        disabled: false,
+        display: {
+          field: 'typeCode',
+          operator: IFormContextConfigOperator.EQUALS,
+          value: 1,
+        },
+        label: 'Имя',
+        name: 'firstName',
+        type: IMetadataFormControlType.TEXT,
+        validators: {},
+        width: 0,
+      },
+      {
+        disabled: false,
+        display: {
+          field: 'typeCode',
+          operator: IFormContextConfigOperator.EQUALS,
+          value: 1,
+        },
+        label: 'Отчество',
+        name: 'middleName',
+        type: IMetadataFormControlType.TEXT,
+        validators: {},
+        width: 0,
+      },
+      {
+        dictCode: UserDictionariesService.DICTIONARY_PERSON_TYPE,
+        disabled: false,
+        display: true,
+        label: 'Тип',
+        name: 'typeCode',
+        type: IMetadataFormControlType.SELECT,
+        validators: {},
+        width: 0,
+      },
+      {
+        disabled: false,
+        display: {
+          field: 'typeCode',
+          operator: IFormContextConfigOperator.EQUALS,
+          value: 1,
+        },
+        label: 'Дата рождения',
+        name: 'birthDate',
+        type: IMetadataFormControlType.DATE,
+        validators: {},
+        width: 0,
+      },
+      {
+        dictCode: UserDictionariesService.DICTIONARY_GENDER,
+        disabled: false,
+        display: {
+          field: 'typeCode',
+          operator: IFormContextConfigOperator.EQUALS,
+          value: 1,
+        },
+        label: 'Пол',
+        name: 'genderCode',
+        type: IMetadataFormControlType.SELECT,
+        validators: {},
+        width: 0,
+      },
+      {
+        disabled: false,
+        display: true,
+        label: 'Серия и номер паспорта',
+        name: 'passportNumber',
+        type: IMetadataFormControlType.TEXT,
+        validators: {},
+        width: 0,
+      },
+      ...range(1, 10).map(i => ({
+        disabled: false,
+        display: {
+          type: IContextConfigItemType.GROUP,
+          operator: IContextConfigOperator.AND,
+          children: [
+            {
+              type: IContextConfigItemType.ENTITY,
+              method: IContextByEntityMethod.IS_USED,
+              value: 363 + i,
+            },
+            {
+              type: IContextConfigItemType.CONSTANT,
+              method: IContextByValueBagMethod.CONTAINS,
+              value: [ 'Person.Individual.AdditionalAttribute.List', 363 + i ],
+            }
+          ],
+        },
+        label: `Строковый атрибут ${i}`,
+        name: `stringValue${i}`,
+        type: IMetadataFormControlType.TEXT,
+        validators: {},
+        width: 0,
+      }) as IMetadataFormTextControl),
+      {
+        disabled: false,
+        display: true,
+        label: 'Комментарий',
+        name: 'comment',
+        type: IMetadataFormControlType.TEXTAREA,
         validators: {},
         width: 0,
       },
