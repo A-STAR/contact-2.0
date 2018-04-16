2af480a4ed3331f6488f7e9df2974550de30f443
Ivan Lobanov <arkhemlol@gmail.com>
2018-04-16 22:17:28 +0300

    chore: fix test

--- src/app/shared/mass-ops/address/address.service.spec.ts
+++ src/app/shared/mass-ops/address/address.service.spec.ts
@@ -1,11 +1,25 @@
 import { TestBed, inject } from '@angular/core/testing';
 
+import { ActionGridService } from '@app/shared/components/action-grid/action-grid.service';
 import { AddressService } from './address.service';
+
+
+class MockActionGridService {
+  buildRequest(): any {
+    return {};
+  }
+}
 
 describe('AddressService', () => {
   beforeEach(() => {
     TestBed.configureTestingModule({
-      providers: [AddressService]
+      providers: [
+        AddressService,
+        {
+          provide: ActionGridService,
+          useClass: MockActionGridService,
+        }
+      ]
     });
   });
 
