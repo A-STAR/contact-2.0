4a182d032daf8a1bf46f5fb0238d45409e4f986d
Ivan Lobanov <arkhemlol@gmail.com>
2018-04-18 18:47:45 +0300

    chore: fix test

--- src/app/shared/mass-ops/address/address.service.spec.ts
+++ src/app/shared/mass-ops/address/address.service.spec.ts
@@ -1,11 +1,27 @@
 import { TestBed, inject } from '@angular/core/testing';
+import { Observable } from 'rxjs/Observable';
+import { of } from 'rxjs/observable/of';
 
 import { ActionGridService } from '@app/shared/components/action-grid/action-grid.service';
 import { AddressService } from './address.service';
+import { DataService } from '@app/core/data/data.service';
+import { NotificationsService } from '@app/core/notifications/notifications.service';
 
 
 class MockActionGridService {
   buildRequest(): any {
+    return {};
+  }
+}
+
+class MockDataService {
+  create(): Observable<any> {
+    return of([]);
+  }
+}
+
+class MockNotificationsService {
+  fetchError(): any {
     return {};
   }
 }
@@ -18,6 +34,14 @@ describe('AddressService', () => {
         {
           provide: ActionGridService,
           useClass: MockActionGridService,
+        },
+        {
+          provide: DataService,
+          useClass: MockDataService,
+        },
+        {
+          provide: NotificationsService,
+          useClass: MockNotificationsService,
         }
       ]
     });
