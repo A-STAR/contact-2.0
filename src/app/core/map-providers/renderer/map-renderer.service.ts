f1adb594caf903b4bef9fd426933c130b50a2779
Ivan Lobanov <arkhemlol@gmail.com>
2018-04-19 20:14:22 +0300

    fix: cd for map components
    #refs WEB20-1379

--- src/app/core/map-providers/renderer/map-renderer.service.ts
+++ src/app/core/map-providers/renderer/map-renderer.service.ts
@@ -4,6 +4,7 @@ import {
   Injector,
   TemplateRef,
   Type,
+  ApplicationRef,
 } from '@angular/core';
 
 import { IMapComponent, IMapComponentRenderResult } from './map-renderer.interface';
@@ -12,8 +13,9 @@ import { IMapComponent, IMapComponentRenderResult } from './map-renderer.interfa
 export class MapRendererService {
 
   constructor(
-    private resolver: ComponentFactoryResolver,
+    private appRef: ApplicationRef,
     private injector: Injector,
+    private resolver: ComponentFactoryResolver,
   ) { }
 
   render<T extends IMapComponent>(cmp: Type<T>, data: any, tpl?: TemplateRef<any>): IMapComponentRenderResult<T> {
@@ -21,6 +23,7 @@ export class MapRendererService {
     const compFactory = this.resolver.resolveComponentFactory<T>(cmp);
     const divEl = document.createElement('div');
     const compRef = compFactory.create(this.injector, null, divEl);
+    this.appRef.attachView(compRef.hostView);
 
     compRef.instance.context = { data };
     compRef.instance.tpl = tpl;
