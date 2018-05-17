d0b3ab35e981cdf93b043ffe74899ef9000ae1e3
Dmitri Maltsev <dmitri.maltsev@hotmail.com>
2018-05-17 12:48:04 +0300

    feat: debtor card tabs concept

--- src/app/routes/workplaces/debtor-card/debtor.component.ts
+++ src/app/routes/workplaces/debtor-card/debtor.component.ts
@@ -1,9 +1,17 @@
-import { ChangeDetectionStrategy, Component, OnDestroy, OnInit, ViewEncapsulation } from '@angular/core';
+import {
+  ChangeDetectionStrategy,
+  ChangeDetectorRef,
+  Component,
+  OnDestroy,
+  OnInit,
+  ViewEncapsulation,
+} from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
 import { Subscription } from 'rxjs/Subscription';
 
 import { ContactRegistrationService } from '@app/routes/workplaces/shared/contact-registration/contact-registration.service';
 import { DebtorCardService } from '@app/core/app-modules/debtor-card/debtor-card.service';
-import { ActivatedRoute } from '@angular/router';
+import { DebtorService } from './debtor.service';
 
 @Component({
   changeDetection: ChangeDetectionStrategy.OnPush,
@@ -22,22 +30,31 @@ export class DebtorComponent implements OnInit, OnDestroy {
   readonly displayContactRegistration$ = this.contactRegistrationService.isActive$;
 
   constructor(
+    private cdRef: ChangeDetectorRef,
     private contactRegistrationService: ContactRegistrationService,
     private debtorCardService: DebtorCardService,
+    private debtorService: DebtorService,
     private route: ActivatedRoute,
   ) {}
 
   ngOnInit(): void {
     this.routeIdSubscription = this.route.paramMap
       .subscribe(paramMap => {
-        const debtId = paramMap.get('debtId');
+        const debtId   = Number(paramMap.get('debtId'));
+        const debtorId = Number(paramMap.get('debtorId'));
         if (debtId) {
-          this.debtorCardService.initByDebtId(Number(debtId));
+          this.debtorCardService.initByDebtId(debtId);
         }
+        this.debtorService.addTab(debtorId, debtId);
+        this.cdRef.markForCheck();
     });
   }
 
   ngOnDestroy(): void {
     this.routeIdSubscription.unsubscribe();
+  }
+
+  get debtors(): any {
+    return Array.from(this.debtorService.debtors).map(debtor => debtor.split(':'));
   }
 }
--- src/app/routes/workplaces/debtor-card/debtor.service.ts
+++ src/app/routes/workplaces/debtor-card/debtor.service.ts
@@ -8,14 +8,24 @@ import { NotificationsService } from '../../../core/notifications/notifications.
 
 @Injectable()
 export class DebtorService {
+  private _debtors = new Set<string>();
+
   constructor(
     private dataService: DataService,
     private notificationsService: NotificationsService,
   ) {}
 
+  get debtors(): Set<string> {
+    return this._debtors;
+  }
+
   update(id: number, person: IPerson): Observable<void> {
     return this.dataService
       .update('/persons/{id}', { id }, person)
       .catch(this.notificationsService.updateError().entity('entities.persons.gen.singular').dispatchCallback());
+  }
+
+  addTab(debtorId: number, debtId: number): void {
+    this._debtors.add(debtorId + ':' + debtId);
   }
 }
