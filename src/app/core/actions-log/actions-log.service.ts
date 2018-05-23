3b9f8e9fe8707c67b3547a17982a975512756985
Dmitri Maltsev <dmitri.maltsev@hotmail.com>
2018-05-23 12:13:46 +0300

    feat: remove notification on log error

--- src/app/core/actions-log/actions-log.service.ts
+++ src/app/core/actions-log/actions-log.service.ts
@@ -1,24 +1,16 @@
 import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs/Observable';
-import { catchError } from 'rxjs/operators';
 
 import { DataService } from '@app/core/data/data.service';
-import { NotificationsService } from '@app/core/notifications/notifications.service';
 
 @Injectable()
 export class ActionsLogService {
   constructor(
     private dataService: DataService,
-    private notificationsService: NotificationsService,
   ) {}
 
   logOpenAction(duration: number, personId: number): Observable<void> {
     const data = { typeCode: 1, duration, personId };
-    return this.dataService
-      .create('/actions', {}, data)
-      .pipe(
-        // TODO(d.maltsev): i18n
-        catchError(this.notificationsService.error('').dispatchCallback()),
-      );
+    return this.dataService.create('/actions', {}, data);
   }
 }
