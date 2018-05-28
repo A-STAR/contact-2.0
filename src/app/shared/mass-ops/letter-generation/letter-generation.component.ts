fdcfb04d0e9ab44b8de0c0100b89ae85b72d9ea2
Igor Kibisov <donar061@gmail.com>
2018-05-28 10:43:20 +0300

    feat: mass letter personRole param
    #refs WEB20-1432

--- src/app/shared/mass-ops/letter-generation/letter-generation.component.ts
+++ src/app/shared/mass-ops/letter-generation/letter-generation.component.ts
@@ -52,6 +52,8 @@ export class LetterGenerationComponent implements OnInit {
     ignoreWrongAddress: 1
   };
 
+  private personRole: number;
+
   constructor(
     private cdRef: ChangeDetectorRef,
     private actionGridService: ActionGridService,
@@ -61,8 +63,8 @@ export class LetterGenerationComponent implements OnInit {
   ) { }
 
   ngOnInit(): void {
-    const personRole = Number(this.actionGridService.getAddOption(this.actionData, 'personRole', 0));
-    this.userTemplatesService.getLetterTemplatesForDebt(personRole, null)
+    this.personRole = Number(this.actionGridService.getAddOption(this.actionData, 'personRole', 0));
+    this.userTemplatesService.getLetterTemplatesForDebt(this.personRole, null)
       .map(templates => templates.map(t => ({ label: t.name, value: t.id })))
       .subscribe(templateOptions => {
         this.layoutConfig = this.createLayout(templateOptions);
@@ -76,7 +78,7 @@ export class LetterGenerationComponent implements OnInit {
 
   onSubmit(): void {
     this.letterGenerationService
-      .generate(this.actionData.payload, this.layout.getData())
+      .generate(this.actionData.payload, this.personRole, this.layout.getData())
       .subscribe(() => this.close.emit({ refresh: false }));
   }
 
--- src/app/shared/mass-ops/letter-generation/letter-generation.service.ts
+++ src/app/shared/mass-ops/letter-generation/letter-generation.service.ts
@@ -5,25 +5,25 @@ import { catchError } from 'rxjs/operators/catchError';
 import { IGridActionPayload } from '@app/shared/components/action-grid/action-grid.interface';
 import { ILetterGenerationParams } from './letter-generation.interface';
 
-import { ActionGridService } from '@app/shared/components/action-grid/action-grid.service';
 import { DataService } from '@app/core/data/data.service';
 import { NotificationsService } from '@app/core/notifications/notifications.service';
-
 
 @Injectable()
 export class LetterGenerationService {
 
   constructor(
-    private actionGridService: ActionGridService,
     private dataService: DataService,
     private notificationsService: NotificationsService,
   ) {}
 
-  generate(idData: IGridActionPayload, actionData: ILetterGenerationParams): Observable<void> {
+  generate(idData: IGridActionPayload, personRole: number, actionData: ILetterGenerationParams): Observable<void> {
     return this.dataService
       .create('/async/mass/letters/form', {},
         {
-          idData: this.actionGridService.buildRequest(idData),
+          idData: {
+            ids: (idData.data as number[][])
+              .map(rowIds => [ ...rowIds, personRole ])
+          },
           actionData
         }
       )
