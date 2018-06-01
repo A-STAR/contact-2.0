9c26d5a536c34328cbea8bfd9d6e37cacdce7235
Dmitri Maltsev <dmitri.maltsev@hotmail.com>
2018-06-01 17:36:45 +0300

    fix: fetching documents for different entities

--- src/app/routes/workplaces/call-center/campaign/campaign-layout/documents/documents.component.ts
+++ src/app/routes/workplaces/call-center/campaign/campaign-layout/documents/documents.component.ts
@@ -1,5 +1,7 @@
 import { ChangeDetectionStrategy, Component } from '@angular/core';
 
+import { EntityType } from '@app/core/entity/entity.interface';
+
 import { CampaignService } from '../../campaign.service';
 
 @Component({
@@ -10,6 +12,8 @@ import { CampaignService } from '../../campaign.service';
 export class DocumentsComponent {
   readonly debtExists$ = this.campaignService.campaignDebt$.map(Boolean);
   readonly debtId$ = this.campaignService.campaignDebt$.map(debt => debt.debtId);
+
+  readonly entityType = EntityType.DEBT;
 
   constructor(
     private campaignService: CampaignService,
