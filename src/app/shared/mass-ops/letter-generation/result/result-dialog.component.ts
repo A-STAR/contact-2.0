f4902c721ab9c397b4a0b11dd9a01e64703f46ec
Igor Kibisov <donar061@gmail.com>
2018-06-06 15:29:31 +0300

    feat: mass letter result type
    #refs WEB20-1443

--- src/app/shared/mass-ops/letter-generation/result/result-dialog.component.ts
+++ src/app/shared/mass-ops/letter-generation/result/result-dialog.component.ts
@@ -1,9 +1,12 @@
 import { ChangeDetectionStrategy, Component, Output, EventEmitter, ViewChild, OnInit } from '@angular/core';
 
 import { ICloseAction } from '@app/shared/components/action-grid/action-grid.interface';
-import { IDynamicLayoutConfig } from '@app/shared/components/dynamic-layout/dynamic-layout.interface';
+import {
+  IDynamicLayoutConfig, DynamicLayoutControlType, DynamicLayoutItemType
+} from '@app/shared/components/dynamic-layout/dynamic-layout.interface';
 
 import { DynamicLayoutComponent } from '@app/shared/components/dynamic-layout/dynamic-layout.component';
+import { TranslateService } from '@ngx-translate/core';
 
 @Component({
   selector: 'app-letter-generation-result',
@@ -15,7 +18,32 @@ export class LetterGenerationResultComponent implements OnInit {
 
   @Output() close = new EventEmitter<ICloseAction>();
 
-  config: IDynamicLayoutConfig;
+  config: IDynamicLayoutConfig = {
+    key: 'mass/letter/result',
+    items: [
+      {
+        type: DynamicLayoutItemType.CONTROL,
+        controlType: DynamicLayoutControlType.SELECT,
+        label: 'widgets.mass.letter.dialog.resultType',
+        name: 'resultType',
+        options: [
+          {
+            label: this.translateService.instant('widgets.mass.letter.dialog.fileWithLetters'),
+            value: 1
+          },
+          {
+            label: this.translateService.instant('widgets.mass.letter.dialog.register'),
+            value: 2
+          }
+        ]
+      }
+    ]
+  };
+
+  constructor(
+    private translateService: TranslateService
+  ) {
+  }
 
   ngOnInit(): void {
   }
