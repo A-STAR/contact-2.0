24b4956a38f7cd2cb65520132e6154a9e25b4bcf
Igor Kibisov <donar061@gmail.com>
2018-06-13 11:51:46 +0300

    feat: debt processing tabs
    #refs WEB20-1010

--- src/app/routes/workplaces/debt-processing/debt-processing.component.ts
+++ src/app/routes/workplaces/debt-processing/debt-processing.component.ts
@@ -2,6 +2,8 @@ import { ChangeDetectionStrategy, Component } from '@angular/core';
 
 import { ITab } from '../../../shared/components/layout/tabview/header/header.interface';
 
+import { UserPermissionsService } from '@app/core/user/permissions/user-permissions.service';
+
 import { makeKey } from '../../../core/utils';
 
 const label = makeKey('modules.debtsProcessing');
@@ -14,13 +16,41 @@ const label = makeKey('modules.debtsProcessing');
 })
 export class DebtProcessingComponent {
   tabs: ITab[] = [
-    { link: 'all' },
-    { link: 'callBack' },
-    { link: 'currentJob' },
-    { link: 'visits' },
-    { link: 'promisePay' },
-    { link: 'partPay' },
-    { link: 'problem' },
-    { link: 'returned' },
+    {
+      link: 'all',
+      hasPermission: this.userPermissionsService.has('DEBT_PROCESSING_TAB_ALL')
+    },
+    {
+      link: 'callBack',
+      hasPermission: this.userPermissionsService.has('DEBT_PROCESSING_TAB_CALL_BACK')
+    },
+    {
+      link: 'currentJob',
+      hasPermission: this.userPermissionsService.has('DEBT_PROCESSING_TAB_CURRENT_JOB')
+    },
+    {
+      link: 'visits',
+      hasPermission: this.userPermissionsService.has('DEBT_PROCESSING_TAB_VISITS')
+    },
+    {
+      link: 'promisePay',
+      hasPermission: this.userPermissionsService.has('DEBT_PROCESSING_TAB_PROMISE_PAY')
+    },
+    {
+      link: 'partPay',
+      hasPermission: this.userPermissionsService.has('DEBT_PROCESSING_TAB_PART_PAY')
+    },
+    {
+      link: 'problem',
+      hasPermission: this.userPermissionsService.has('DEBT_PROCESSING_TAB_PROBLEM')
+    },
+    {
+      link: 'returned',
+      hasPermission: this.userPermissionsService.has('DEBT_PROCESSING_TAB_RETURN')
+    },
   ].map(tab => ({ ...tab, title: label(`${tab.link}.title`) }));
+
+  constructor(
+    private userPermissionsService: UserPermissionsService
+  ) { }
 }
