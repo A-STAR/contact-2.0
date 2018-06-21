7b277d58167959aaa2d1c81b23925f6bb1a5237a
Dmitri Maltsev <dmitri.maltsev@hotmail.com>
2018-06-21 14:55:42 +0300

    refactoring: custom operation params service

--- src/app/shared/mass-ops/custom-operation/params/custom-operation-params.component.ts
+++ src/app/shared/mass-ops/custom-operation/params/custom-operation-params.component.ts
@@ -12,27 +12,21 @@ import {
 import { DomSanitizer, SafeUrl } from '@angular/platform-browser';
 import { BehaviorSubject } from 'rxjs/BehaviorSubject';
 import { Subscription } from 'rxjs/Subscription';
-import { fromEvent } from 'rxjs/observable/fromEvent';
-import { filter, map, shareReplay } from 'rxjs/operators';
 
 import { ICustomOperationParams } from '../custom-operation.interface';
 import { IDynamicLayoutConfig } from '@app/shared/components/dynamic-layout/dynamic-layout.interface';
-import {
-  FrameMessageDirection,
-  FrameMessageType,
-  IFrameRequestMessage,
-  IFrameResponseMessage,
-} from './custom-operation-params.interface';
 
 import { ConfigService } from '@app/core/config/config.service';
+import { CustomOperationParamsService } from '@app/shared/mass-ops/custom-operation/params/custom-operation-params.service';
 import { CustomOperationService } from '@app/shared/mass-ops/custom-operation/custom-operation.service';
-import { LookupService } from '@app/core/lookup/lookup.service';
-import { UserDictionariesService } from '@app/core/user/dictionaries/user-dictionaries.service';
 
 import { DynamicLayoutComponent } from '@app/shared/components/dynamic-layout/dynamic-layout.component';
 
 @Component({
   changeDetection: ChangeDetectionStrategy.OnPush,
+  providers: [
+    CustomOperationParamsService,
+  ],
   selector: 'app-custom-operation-params',
   styleUrls: [ './custom-operation-params.component.scss' ],
   templateUrl: './custom-operation-params.component.html',
@@ -52,20 +46,12 @@ export class CustomOperationParamsComponent implements OnInit, AfterViewInit, On
   private canSubmit$ = new BehaviorSubject<boolean>(false);
   private canSubmitSub: Subscription;
 
-  private messages$ = fromEvent(window, 'message')
-    .pipe(
-      map((message: MessageEvent) => message.data as IFrameRequestMessage),
-      filter(message => message.direction === FrameMessageDirection.REQUEST && message.operationId === this.id),
-      shareReplay(),
-    );
-
   constructor(
     private cdRef: ChangeDetectorRef,
     private configService: ConfigService,
+    private customOperationParamsService: CustomOperationParamsService,
     private customOperationService: CustomOperationService,
     private domSanitizer: DomSanitizer,
-    private lookupService: LookupService,
-    private userDictionariesService: UserDictionariesService,
   ) {}
 
   get canSubmit(): boolean {
@@ -93,18 +79,10 @@ export class CustomOperationParamsComponent implements OnInit, AfterViewInit, On
           this.cdRef.markForCheck();
         });
     }
-
-    this.messages$.subscribe(message => {
-      switch (message.type) {
-        case FrameMessageType.DICTIONARY:
-          this.onDictionaryMessage(message.params);
-          break;
-        case FrameMessageType.INIT:
-          this.onInitMessage();
-          break;
-        case FrameMessageType.LOOKUP:
-          this.onLookupMessage(message.params);
-          break;
+    this.customOperationParamsService.init(this.id, this.params);
+    this.customOperationParamsService.messages$.subscribe(message => {
+      if (this.frame && this.frame.nativeElement.contentWindow) {
+        this.frame.nativeElement.contentWindow.postMessage(message, '*');
       }
     });
   }
@@ -113,49 +91,5 @@ export class CustomOperationParamsComponent implements OnInit, AfterViewInit, On
     if (this.canSubmitSub) {
       this.canSubmitSub.unsubscribe();
     }
-  }
-
-  private onDictionaryMessage(params: any): void {
-    this.userDictionariesService
-      .getDictionary(params.code)
-      .subscribe(payload => {
-        const message: IFrameResponseMessage = {
-          type: FrameMessageType.DICTIONARY,
-          direction: FrameMessageDirection.RESPONSE,
-          operationId: this.id,
-          params,
-          payload,
-        };
-        this.frame.nativeElement.contentWindow.postMessage(message, '*');
-      });
-  }
-
-  private onInitMessage(): void {
-    const message: IFrameResponseMessage = {
-      type: FrameMessageType.INIT,
-      direction: FrameMessageDirection.RESPONSE,
-      operationId: this.id,
-      params: null,
-      payload: {
-        params: this.params,
-        value: this.value,
-      },
-    };
-    this.frame.nativeElement.contentWindow.postMessage(message, '*');
-  }
-
-  private onLookupMessage(params: any): void {
-    this.lookupService
-      .lookup(params.code)
-      .subscribe(payload => {
-        const message: IFrameResponseMessage = {
-          type: FrameMessageType.LOOKUP,
-          direction: FrameMessageDirection.RESPONSE,
-          operationId: this.id,
-          params,
-          payload,
-        };
-        this.frame.nativeElement.contentWindow.postMessage(message, '*');
-      });
   }
 }
