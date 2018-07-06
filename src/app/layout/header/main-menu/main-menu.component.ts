b841c226f66d4470c000435315fe677f8ba06cc3
Dmitri Maltsev <dmitri.maltsev@hotmail.com>
2018-07-06 18:49:12 +0300

    feat: help button

--- src/app/layout/header/main-menu/main-menu.component.ts
+++ src/app/layout/header/main-menu/main-menu.component.ts
@@ -1,7 +1,11 @@
 import { ChangeDetectionStrategy, Component } from '@angular/core';
 import { map } from 'rxjs/operators';
 
+import { MenuItemType } from '@app/routes/menu-config.interface';
+import { IMenuItem } from '@app/core/gui-objects/gui-objects.interface';
+
 import { GuiObjectsService } from '@app/core/gui-objects/gui-objects.service';
+import { HelpService } from '@app/core/help/help.service';
 
 @Component({
   selector: 'app-header-main-menu',
@@ -9,11 +13,21 @@ import { GuiObjectsService } from '@app/core/gui-objects/gui-objects.service';
   changeDetection: ChangeDetectionStrategy.OnPush
 })
 export class MainMenuComponent {
+
   readonly menuItems$ = this.guiObjectsService.menuItems.pipe(
     map(items => items.filter(item => item.text)),
   );
 
   constructor(
-    private guiObjectsService: GuiObjectsService
+    private guiObjectsService: GuiObjectsService,
+    private helpService: HelpService,
   ) {}
+
+  isHelpItem(item: IMenuItem): boolean {
+    return item.type === MenuItemType.HELP;
+  }
+
+  onHelp(): void {
+    this.helpService.open('');
+  }
 }
