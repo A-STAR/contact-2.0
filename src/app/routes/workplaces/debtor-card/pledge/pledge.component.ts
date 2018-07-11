528fbb411b70cb9cdf4f05d9c3fd8cf8c53f5242
Ivan Lobanov <arkhemlol@gmail.com>
2018-07-11 13:28:07 +0300

    WEB20-1757 fix: splitter inside tabs

--- src/app/routes/workplaces/debtor-card/pledge/pledge.component.ts
+++ src/app/routes/workplaces/debtor-card/pledge/pledge.component.ts
@@ -1,15 +1,38 @@
-import { Component, ChangeDetectionStrategy } from '@angular/core';
+import {
+  Component,
+  ChangeDetectionStrategy,
+  ChangeDetectorRef,
+  ViewChild,
+  AfterViewInit,
+  QueryList,
+  ViewChildren,
+} from '@angular/core';
 import { UserPermissionsService } from '@app/core/user/permissions/user-permissions.service';
+
+import { SplitComponent, SplitAreaDirective } from 'angular-split';
 
 @Component({
   selector: 'app-pledge',
   templateUrl: './pledge.component.html',
   changeDetection: ChangeDetectionStrategy.OnPush,
+  host: { class: 'full-size' }
 })
-export class PledgeComponent {
+export class PledgeComponent implements AfterViewInit {
+
+  @ViewChild(SplitComponent) splitEl: SplitComponent;
+  @ViewChildren(SplitAreaDirective) areas: QueryList<SplitAreaDirective>;
+
   constructor(
+    private cdRef: ChangeDetectorRef,
     private userPermissionsService: UserPermissionsService,
   ) { }
+
+  ngAfterViewInit(): void {
+    setTimeout(() => {
+      this.areas.forEach(a => this.splitEl.updateArea(a, true, true));
+      this.cdRef.markForCheck();
+    }, 0);
+  }
 
   readonly displayAttributes$ = this.userPermissionsService.contains('ATTRIBUTE_VIEW_LIST', 32);
 }
--- src/app/routes/workplaces/debtor-card/property/property.component.ts
+++ src/app/routes/workplaces/debtor-card/property/property.component.ts
@@ -1,16 +1,40 @@
-import { Component, ChangeDetectionStrategy, Input } from '@angular/core';
+import {
+  Component,
+  ChangeDetectionStrategy,
+  Input,
+  ViewChildren,
+  ViewChild,
+  QueryList,
+  ChangeDetectorRef,
+  AfterViewInit,
+} from '@angular/core';
+
 import { UserPermissionsService } from '@app/core/user/permissions/user-permissions.service';
+
+import { SplitComponent, SplitAreaDirective } from 'angular-split';
 
 @Component({
   selector: 'app-property',
   templateUrl: './property.component.html',
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
-export class PropertyComponent {
+export class PropertyComponent implements AfterViewInit {
+
+  @ViewChild(SplitComponent) splitEl: SplitComponent;
+  @ViewChildren(SplitAreaDirective) areas: QueryList<SplitAreaDirective>;
+
   @Input() personId: number;
   constructor(
+    private cdRef: ChangeDetectorRef,
     private userPermissionsService: UserPermissionsService,
   ) { }
+
+  ngAfterViewInit(): void {
+    setTimeout(() => {
+      this.areas.forEach(a => this.splitEl.updateArea(a, true, true));
+      this.cdRef.markForCheck();
+    }, 0);
+  }
 
   readonly displayAttributes$ = this.userPermissionsService.contains('ATTRIBUTE_VIEW_LIST', 33);
 
