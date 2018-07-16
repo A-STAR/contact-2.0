ed80943af59fcc0b9aee6f8eaf4b127f0b8190e6
Dmitri Maltsev <dmitri.maltsev@hotmail.com>
2018-07-16 15:31:48 +0300

    fix: pledge property form control type
    #refs WEB20-1788

--- src/app/routes/workplaces/debtor-card/pledge/card/create-contract/create-contract.layout.ts
+++ src/app/routes/workplaces/debtor-card/pledge/card/create-contract/create-contract.layout.ts
@@ -329,14 +329,14 @@ export const createContractLayout: IDynamicLayoutConfig = {
                 },
                 {
                   type: DynamicLayoutItemType.CONTROL,
-                  controlType: DynamicLayoutControlType.TEXT,
+                  controlType: DynamicLayoutControlType.NUMBER,
                   form: 'propertyValue',
                   label: 'routes.workplaces.debtorCard.pledge.card.forms.property.form.pledgeValue',
                   name: 'pledgeValue',
                 },
                 {
                   type: DynamicLayoutItemType.CONTROL,
-                  controlType: DynamicLayoutControlType.TEXT,
+                  controlType: DynamicLayoutControlType.NUMBER,
                   form: 'propertyValue',
                   label: 'routes.workplaces.debtorCard.pledge.card.forms.property.form.marketValue',
                   name: 'marketValue',
