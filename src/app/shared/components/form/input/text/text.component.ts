1d2ac0981763e5bac86c7858a1638122ba7a7730
Dmitri Maltsev <dmitri.maltsev@hotmail.com>
2018-07-16 14:44:13 +0300

    feat: grid quick filter

--- src/app/shared/components/form/input/text/text.component.ts
+++ src/app/shared/components/form/input/text/text.component.ts
@@ -28,6 +28,7 @@ export class TextComponent implements ControlValueAccessor, Validator, AfterView
   @Input() label: string;
   @Input() minLength: number;
   @Input() maxLength: number;
+  @Input() placeholder: string;
   @Input() required = false;
   @Input() autofocus = false;
 
